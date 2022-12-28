606fa6e6f599dad39253a61202ff8d752fba1306
guybrush <realguybrush@vivaldi.net>
2022-12-28 13:37:08 +0700

    renumber drills with extra zero padding; add frogs directory for each section

--- cards/rotations-violin.sh
+++ cards/rotations-violin.sh
@@ -18,83 +18,87 @@ SCALE_VARS="timing=together,predictive tempo=30,60,90,120,240 npb=1,2,3,4,5,6,7 
 
 # build the drill hierarchy
 mkdir violin; cd violin
-drill 001.ear/001.pitch-singing 
-drill 001.ear/002.pitch-matching timing=after,together,before
-drill 001.ear/003.interval-singing
-drill 001.ear/004.interval-matching timing=after,together,before
-drill 001.ear/005.interval-recognition
-drill 001.ear/006.double-stop-matching
-drill 001.ear/007.retuning
-drill 002.frogs/001.fingertip-kisses
-drill 002.frogs/002.pinky-reaches
-drill 002.frogs/003.pinky-vibrato $ES
-drill 003.bow-arm/002.bow-hold/001.jellyfish
-drill 003.bow-arm/002.bow-hold/002.itsy-bitsy-spider
-drill 003.bow-arm/002.bow-hold/003.windscreen-wiper
-drill 003.bow-arm/002.bow-hold/004.bow-hand-resets
-drill 003.bow-arm/002.bow-hold/005.horizontal-bow-raises
-drill 003.bow-arm/002.bow-hold/006.vertical-bow-raises
-drill 003.bow-arm/003.bow-placement/001.square $MFT $ESDS
-drill 003.bow-arm/003.bow-placement/002.swivel-and-stop $MFT $ES
-drill 003.bow-arm/003.bow-placement/003.hops $MFTZ
-drill 003.bow-arm/003.bow-placement/004.circles dir=u,d $MFTZ
-drill 003.bow-arm/003.bow-placement/005.pizzicato
-drill 003.bow-arm/004.bow-pressure/001.bow-benders $ESDS
-drill 003.bow-arm/004.bow-pressure/002.one-minute-bows $ESDS
-drill 003.bow-arm/004.bow-pressure/003.even-bowing $ESDS spb=2,4,8,15
-drill 003.bow-arm/004.bow-pressure/004.frog-reversed $ESDS spb=2,4,8,15
-drill 003.bow-arm/004.bow-pressure/005.finger-lifting $ESDS spb=2,4,8,15
-drill 003.bow-arm/004.bow-pressure/006.pinky-and-index $ESDS spb=2,4,8,15
-drill 003.bow-arm/004.bow-pressure/007.air-bowing spb=4,8,15 bow=normal,reversed
-drill 003.bow-arm/004.bow-pressure/008.parlando $ESDS xpb=1,2,4 first=light,heavy
-drill 003.bow-arm/004.bow-pressure/009.dynamics $ESDS crescendo,decrescendo,pianissimo
-drill 003.bow-arm/005.bow-changes/001.stationary $ESDS $MFT
-drill 003.bow-arm/005.bow-changes/002.detache $ESDS $MFTZ rhythms
-drill 003.bow-arm/005.bow-changes/003.legato $ESDS $MFTZ rhythms
-drill 003.bow-arm/005.bow-changes/004.martele $ESDS $MFTZ
-drill 003.bow-arm/005.bow-changes/005.stacatto $ESDS $MFTZ
-drill 003.bow-arm/005.bow-changes/006.spicatto $ESDS $MFTZ
-drill 003.bow-arm/005.bow-changes/007.colle $ESDS
-drill 003.bow-arm/006.bow-acceleration/001.fast-bowing $ESDS rest=1,00 bps=1,2
-drill 003.bow-arm/006.bow-acceleration/002.speed-changes $ESDS xpb=1,2,3 first=slow,fast
-drill 003.bow-arm/007.bow-direction/001.overbowing
-drill 003.bow-arm/007.bow-direction/002.snaking xpb=1,2,3
-drill 003.bow-arm/007.bow-direction/003.lane-changes lanes=121,232,123,321
-drill 003.bow-arm/007.bow-direction/004.tilt-flat-tilt $ES
-drill 003.bow-arm/008.string-crossings/001.single $XING_VARS num=2,3,4
-drill 003.bow-arm/008.string-crossings/002.double $XING_VARS
-drill 003.bow-arm/008.string-crossings/003.triple $XING_VARS
-drill 003.bow-arm/008.string-crossings/004.chords $XING_VARS pat=1-23,12-3,12-23,12-24
-drill 004.pitch-arm/001.violin-hold/001.no-hand-swivels
-drill 004.pitch-arm/001.violin-hold/002.pinky-reaches
-drill 004.pitch-arm/002.finger-independence/001.finger-wiggles straight,curled
-drill 004.pitch-arm/002.finger-independence/002.fingertip-kisses
-drill 004.pitch-arm/002.finger-independence/003.finger-position-jankin
-drill 004.pitch-arm/002.finger-independence/004.finger-position-curls
-drill 004.pitch-arm/002.finger-independence/005.finger-hammers
-drill 004.pitch-arm/003.finger-dropping/001.trills fingers=001,12,23,34,13,24,14 $ES pos=1,3,5,7,9,Z
-drill 004.pitch-arm/003.finger-dropping/002.half-scales shape=westside,chicken,gun,porcupine $ES
-drill 004.pitch-arm/003.finger-dropping/003.fifth-hops $EF $ES
-drill 004.pitch-arm/003.finger-dropping/004.double-stop-trills $ES thirds
-drill 004.pitch-arm/003.finger-dropping/005.drop-and-lift-to-harmonic $EF $ES
-drill 004.pitch-arm/005.vibrato/001.knocking fulcrum=wrist,arm loc=air,in-situ
-drill 004.pitch-arm/005.vibrato/002.slides fulcrum=wrist,arm $ES
-drill 004.pitch-arm/005.vibrato/003.on-the-body fulcrum=wrist,arm $EF
-drill 004.pitch-arm/005.vibrato/004.on-the-string fulcrum=wrist,arm silent,voiced $EF $ES pos=1,3,5,7,9,Z
-drill 004.pitch-arm/006.shifts/001.one-finger-scales chromatic,major,minor
-drill 004.pitch-arm/006.shifts/002.same-finger-shifts $EF int=1,2,3,4,5,6,7,8,9,X,Y,Z pos=1,3,5,7,9,Z
-drill 004.pitch-arm/006.shifts/003.different-finger-shifts $EF int=1,2,3,4,5,6,7,8,9,X,Y,Z pos=1,3,5,7,9,Z
-drill 005.both-arms/001.chromatic-scale $SCALE_VARS
-scale 005.both-arms/002.scales 001.ud $MODES
-scale 005.both-arms/002.scales 002.climbs $MODES
-scale 005.both-arms/002.scales 003.four-in-a-row $MODES
-scale 005.both-arms/002.scales 004.random-conj $MODES
-scale 005.both-arms/002.scales 005.random-disj $MODES
-scale 005.both-arms/003.arpeggios 001.ud mode=maj,min
-scale 005.both-arms/003.arpeggios 002.climbs mode=maj,min
-scale 005.both-arms/003.arpeggios 003.random-conj mode=maj,min
-scale 005.both-arms/003.arpeggios 004.random-disj mode=maj,min
+drill 0001.A4
+drill 0002.ear/0000.frogs
+drill 0002.ear/0001.pitch-singing 
+drill 0002.ear/0002.pitch-matching timing=after,together,before
+drill 0002.ear/0003.interval-singing
+drill 0002.ear/0004.interval-matching timing=after,together,before
+drill 0002.ear/0005.interval-recognition
+drill 0002.ear/0006.double-stop-matching
+drill 0002.ear/0007.retuning
+drill 0003.bow-arm/0000.frogs
+drill 0003.bow-arm/0001.bow-hold/0001.jellyfish
+drill 0003.bow-arm/0001.bow-hold/0002.itsy-bitsy-spider
+drill 0003.bow-arm/0001.bow-hold/0003.windscreen-wiper
+drill 0003.bow-arm/0001.bow-hold/0004.bow-hand-resets
+drill 0003.bow-arm/0001.bow-hold/0005.horizontal-bow-raises
+drill 0003.bow-arm/0001.bow-hold/0006.vertical-bow-raises
+drill 0003.bow-arm/0002.bow-placement/0001.square $MFT $ESDS
+drill 0003.bow-arm/0002.bow-placement/0002.swivel-and-stop $MFT $ES
+drill 0003.bow-arm/0002.bow-placement/0003.hops $MFTZ
+drill 0003.bow-arm/0002.bow-placement/0004.circles dir=u,d $MFTZ
+drill 0003.bow-arm/0002.bow-placement/0005.pizzicato
+drill 0003.bow-arm/0003.bow-pressure/0001.bow-benders $ESDS
+drill 0003.bow-arm/0003.bow-pressure/0002.one-minute-bows $ESDS
+drill 0003.bow-arm/0003.bow-pressure/0003.even-bowing $ESDS spb=2,4,8,15
+drill 0003.bow-arm/0003.bow-pressure/0004.frog-reversed $ESDS spb=2,4,8,15
+drill 0003.bow-arm/0003.bow-pressure/0005.finger-lifting $ESDS spb=2,4,8,15
+drill 0003.bow-arm/0003.bow-pressure/0006.pinky-and-index $ESDS spb=2,4,8,15
+drill 0003.bow-arm/0003.bow-pressure/0007.air-bowing spb=4,8,15 bow=normal,reversed
+drill 0003.bow-arm/0003.bow-pressure/0008.parlando $ESDS xpb=1,2,4 first=light,heavy
+drill 0003.bow-arm/0003.bow-pressure/0009.dynamics $ESDS crescendo,decrescendo,pianissimo
+drill 0003.bow-arm/0004.bow-changes/0001.stationary $ESDS $MFT
+drill 0003.bow-arm/0004.bow-changes/0002.detache $ESDS $MFTZ rhythms
+drill 0003.bow-arm/0004.bow-changes/0003.legato $ESDS $MFTZ rhythms
+drill 0003.bow-arm/0004.bow-changes/0004.martele $ESDS $MFTZ
+drill 0003.bow-arm/0004.bow-changes/0005.stacatto $ESDS $MFTZ
+drill 0003.bow-arm/0004.bow-changes/0006.spicatto $ESDS $MFTZ
+drill 0003.bow-arm/0004.bow-changes/0007.colle $ESDS
+drill 0003.bow-arm/0005.bow-acceleration/0001.fast-bowing $ESDS rest=1,0 bps=1,2
+drill 0003.bow-arm/0005.bow-acceleration/0002.speed-changes $ESDS xpb=1,2,3 first=slow,fast
+drill 0003.bow-arm/0006.bow-direction/0001.overbowing
+drill 0003.bow-arm/0006.bow-direction/0002.snaking xpb=1,2,3
+drill 0003.bow-arm/0006.bow-direction/0003.lane-changes lanes=121,232,123,321
+drill 0003.bow-arm/0006.bow-direction/0004.tilt-flat-tilt $ES
+drill 0003.bow-arm/0007.string-crossings/0001.single $XING_VARS num=2,3,4
+drill 0003.bow-arm/0007.string-crossings/0002.double $XING_VARS
+drill 0003.bow-arm/0007.string-crossings/0003.triple $XING_VARS
+drill 0003.bow-arm/0007.string-crossings/0004.chords $XING_VARS pat=1-23,12-3,12-23,12-24
+drill 0004.pitch-arm/0000.frogs
+drill 0004.pitch-arm/0001.violin-hold/0001.no-hand-swivels
+drill 0004.pitch-arm/0001.violin-hold/0002.pinky-reaches
+drill 0004.pitch-arm/0002.finger-independence/0001.finger-wiggles straight,curled
+drill 0004.pitch-arm/0002.finger-independence/0002.fingertip-kisses
+drill 0004.pitch-arm/0002.finger-independence/0003.finger-position-jankin
+drill 0004.pitch-arm/0002.finger-independence/0004.finger-position-curls
+drill 0004.pitch-arm/0002.finger-independence/0005.finger-hammers
+drill 0004.pitch-arm/0003.finger-dropping/0001.trills fingers=01,12,23,34,13,24,14 $ES pos=1,3,5,7,9,Z
+drill 0004.pitch-arm/0003.finger-dropping/0002.half-scales shape=westside,chicken,gun,porcupine $ES
+drill 0004.pitch-arm/0003.finger-dropping/0003.fifth-hops $EF $ES
+drill 0004.pitch-arm/0003.finger-dropping/0004.double-stop-trills $ES thirds
+drill 0004.pitch-arm/0003.finger-dropping/0005.drop-and-lift-to-harmonic $EF $ES
+drill 0004.pitch-arm/0005.vibrato/0001.knocking fulcrum=wrist,arm loc=air,in-situ
+drill 0004.pitch-arm/0005.vibrato/0002.slides fulcrum=wrist,arm $ES
+drill 0004.pitch-arm/0005.vibrato/0003.on-the-body fulcrum=wrist,arm $EF
+drill 0004.pitch-arm/0005.vibrato/0004.on-the-string fulcrum=wrist,arm silent,voiced $EF $ES pos=1,3,5,7,9,Z
+drill 0004.pitch-arm/0006.shifts/0001.one-finger-scales chromatic,major,minor
+drill 0004.pitch-arm/0006.shifts/0002.same-finger-shifts $EF int=1,2,3,4,5,6,7,8,9,X,Y,Z pos=1,3,5,7,9,Z
+drill 0004.pitch-arm/0006.shifts/0003.different-finger-shifts $EF int=1,2,3,4,5,6,7,8,9,X,Y,Z pos=1,3,5,7,9,Z
+drill 0005.both-arms/0000.frogs
+drill 0005.both-arms/0001.chromatic-scale $SCALE_VARS
+scale 0005.both-arms/0002.scales 0001.ud $MODES
+scale 0005.both-arms/0002.scales 0002.climbs $MODES
+scale 0005.both-arms/0002.scales 0003.four-in-a-row $MODES
+scale 0005.both-arms/0002.scales 0004.random-conj $MODES
+scale 0005.both-arms/0002.scales 0005.random-disj $MODES
+scale 0005.both-arms/0003.arpeggios 0001.ud mode=maj,min
+scale 0005.both-arms/0003.arpeggios 0002.climbs mode=maj,min
+scale 0005.both-arms/0003.arpeggios 0003.random-conj mode=maj,min
+scale 0005.both-arms/0003.arpeggios 0004.random-disj mode=maj,min
+drill 0006.phrases/0000.frogs
+drill 0007.pieces
 
-#chord patterns {300} {es} {3-strings/12, 3-strings/22} {ud} {fmt}
+#chord patterns {301} {es} {3-strings/12, 3-strings/22} {ud} {fmt}
 #right hand pizzicato
 
