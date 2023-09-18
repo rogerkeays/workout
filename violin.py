af934de6f5a1391a60e78b1eb0f242ff493c364a
guybrush <realguybrush@vivaldi.net>
2023-09-18 15:15:36 +0700

    add baby steps etude

--- violin.py
+++ violin.py
@@ -19,29 +19,51 @@ def the_car_trip(tempo=110):
   single_string_xings("middle", "elbow", "detache", tempo)
   make_card(locals())
 
+#-------------------
 def baby_steps(tempo=90):
   baby_steps(45) if tempo == 90 else 0
-  single_string_xings("middle", "elbow", "detache", tempo)
+  left_foot_step(tempo)
+  right_foot_step(tempo)
+  looks_for_mummy(tempo)
+  falling_down(tempo)
   make_card(locals())
 
+def left_foot_step(tempo=90, rhythm="sssx", melody="00/7", reps=5):
+  string_xings(2, 1, "middle", "elbow", "detache", tempo)
+  make_card(locals())
+
+def right_foot_step(tempo=90, rhythm="sssx", melody="/22\\7", mechanics="U", reps=5):
+  string_xings(3, 2, "middle", "elbow", "detache", tempo)
+  make_card(locals())
+
+def looks_for_mummy(tempo=90, rhythm="ssss", melody="/2299", reps=5):
+  string_xings(3, 4, "middle", "elbow", "detache", tempo)
+  make_card(locals())
+
+def falling_down(tempo=90, rhythm="sssx", melody="\\270", reps=5):
+  string_xings(3, 2, "middle", "elbow", "detache", tempo)
+  string_xings(1, 2, "middle", "elbow", "detache", tempo)
+  make_card(locals())
+
+#-------------------
 def the_crawl(tempo=90, reps=2):
   the_crawl(45) if tempo == 90 else 0
   little_baby_crawls_to_danger(tempo)
   scared_he_turns_round_in_a_circle(tempo)
   make_card(locals())
 
-def little_baby_crawls_to_danger(tempo=90, rhythm="ssss ssss", melody="00 /77 /22 /99", reps=5):
+def little_baby_crawls_to_danger(tempo=90, rhythm="ssss ssss", melody="00 /77 22 99", reps=5):
   string_xings(3, 4, "middle", "elbow", "detache", tempo)
   string_xings(2, 3, "middle", "elbow", "detache", tempo)
   string_xings(1, 2, "middle", "elbow", "detache", tempo)
   make_card(locals())
 
-def scared_he_turns_round_in_a_circle(tempo=90, rhythm="ssss ssss", melody="\\22 \\77 /22 \\77", reps=5):
+def scared_he_turns_round_in_a_circle(tempo=90, rhythm="ssss ssss", melody="\\22 77 /22 \\77", reps=5):
   string_xings(2, 1, "middle", "elbow", "detache", tempo)
   string_xings(3, 2, "middle", "elbow", "detache", tempo)
   make_card(locals())
 
-# drills
+#--------------------
 def single_string_xings(section, fulcrum, attack, tempo):
   string_xings(3, 2, section, fulcrum, attack, tempo)
   string_xings(2, 3, section, fulcrum, attack, tempo)
