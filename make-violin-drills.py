60e15941787a9144465d1df3b5cdb3ccc9d70539
guybrush <realguybrush@vivaldi.net>
2023-11-03 21:47:15 +0700

    amend lightly row bow directions

--- make-violin-drills.py
+++ make-violin-drills.py
@@ -17,9 +17,9 @@ for tempo in [45, 90]:
   piece(tempo, "lightly-row",
     phrase(tempo, "lightly row, flamingo", "QQHQQH", "2", "W", "2", "422311", start=6.92, stop=10.52),
     phrase(tempo, "down the river we will go", "QQQQQQH", "2", "W", "2", "0123444", start=10.52, stop=14.12),
-    phrase(tempo, "always rowing, never slowing", "QQQQQQQQ", "2", "W", "2", "42223111", start=14.12, stop=17.60),
-    phrase(tempo, "in my bright new red canoe", "QQQQQQH", "2", "W", "2", "0244222", start=17.60, stop=21.23),
+    phrase(tempo, "always rowing, never slowing", "QQQQQQQQ", "2", "W", "2", "42223111", "UD", start=14.12, stop=17.60),
+    phrase(tempo, "in my bright new red canoe", "QQQQQQH", "2", "W", "2", "0244222", "UD", start=17.60, stop=21.23),
     phrase(tempo, "see the fishes swimming by", "QQQQQQH", "2", "W", "2", "1111123", start=21.23, stop=24.85),
-    phrase(tempo, "see the birds up in the sky", "QQQQQQH", "2", "W", "2", "2222234", start=24.85, stop=28.58)
+    phrase(tempo, "see the birds up in the sky", "QQQQQQH", "2", "W", "2", "2222234", "UD", start=24.85, stop=28.58)
   )
 
