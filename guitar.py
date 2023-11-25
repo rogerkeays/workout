a070cccad14344a2f71536a79e742222c09f78a5
guybrush <realguybrush@vivaldi.net>
2023-11-25 20:48:50 +0700

    add basic guitar drills: strumming, chords, chord changes

--- guitar.py
+++ guitar.py
@@ -3,7 +3,42 @@
 
 from workout import *
 
-def strumming(tempo, lyrics, rhythm, direction, chords, start, stop):
-  del start, stop
+def strumming(tempo, lyrics, rhythm, directions, chords, start, stop):
+
+  # capture parameters
+  params = locals()
+  del params["start"]
+  del params["stop"]
+
+  # scan phrase
+  n = len(rhythm)
+  for i in range(n):
+    chord_shape(chords[i])
+
+    # transition drills
+    if i > 0:
+      h = i - 1
+      j = i + 1
+      if chords[i] != chords[h]:
+        chord_changes(tempo, chords[h], chords[i])
+
+  # whole phrase
+  muted_strumming(tempo, rhythm, directions)
+
+  make_card(params, 5)
+  make_metronome(tempo)
+  make_chunk(start, stop, tempo)
+
+def muted_strumming(tempo, rhythm, directions):
+  make_card(locals(), 5)
+  make_metronome(tempo)
+
+def chord_changes(tempo, from_chord, to_chord):
+  chord_shape(from_chord)
+  chord_shape(to_chord)
+  make_card(locals(), 15)
+  make_metronome(tempo)
+
+def chord_shape(chord):
   make_card(locals(), 15)
 
