57c902c27b83e0a86f71691f5f86f0ba25a8681f
guybrush <realguybrush@vivaldi.net>
2023-11-25 19:55:36 +0700

    add stubs for guitar workout

---
+++ guitar.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# vim: nowrap
+
+from workout import *
+
+def strumming(tempo, lyrics, rhythm, direction, chords, start, stop):
+  del start, stop
+  make_card(locals(), 15)
+
