91d6a73717d94a3135287eb37079cd0ec012b07b
guybrush <realguybrush@vivaldi.net>
2023-11-30 22:59:23 +0700

    improve output logic: single directory with numbered goals, no repeated cards between goals, metronomes at the end

--- workout.py
+++ workout.py
@@ -7,23 +7,21 @@ SLOW = 60
 MAKE_MP3S = True
 
 # global state
-goalshome = "drills"
-goaldir = ""
+goaldir = "practise"
 goalnum = 0
+goalname = ""
 goaltempo = 90
 goalmp3 = ""
 cards = set()
 mp3s = set()
 
 def goal(name, tempo=90, mp3=""):
-  global goaldir, goalnum, goalmp3, goaltempo
-  goaldir = goalshome + "/" + str(goalnum).zfill(2) + "." + name
+  global goalnum, goalname, goaltempo, goalmp3
   os.makedirs(goaldir, exist_ok=True)
   goalnum += 1
-  goalmp3 = mp3
+  goalname = name
   goaltempo = tempo
-  cards.clear()
-  mp3s.clear()
+  goalmp3 = mp3
 
 def piece(tempo, name, *deps):
   del deps
@@ -38,13 +36,14 @@ def make_card(params = {}, reps=5):
   if key not in cards:
     cards.add(key)
     with open(goaldir + "/" + drillnum() + "A.txt", "w") as f:
+      f.writelines(goalname + "\n")
       f.writelines(name + "\n")
       for key in params:
         if params[key]: f.write(key + "=" + str(params[key]) + "\n")
       f.write("reps=" + str(reps))
 
 def drillnum():
-  return str(len(cards)).zfill(4)
+  return str(goalnum).zfill(2) + str(len(cards)).zfill(3)
 
 def make_metronome(tempo):
   make_mp3("""
@@ -56,7 +55,7 @@ def make_metronome(tempo):
   %%MIDI program 115
   |:cccc|cccc|cccc|cccc|cccc|cccc|cccc|cccc:|
   |:cccc|cccc|cccc|cccc|cccc|cccc|cccc|cccc:|
-  """, 0, tempo, name=str(tempo))
+  """, 0, tempo, filename="X" + str(tempo).zfill(5) + ".mp3")
 
 #
 # use MIDI instrument number (abc instrument number is zero-based)
@@ -71,17 +70,17 @@ def make_drone(note, instrument=57):
   K:C transpose={transpose}
   %%MIDI program {instrument}
   |C,,,,|
-  """.format(transpose=note_to_decimal(note), instrument=instrument - 1), name=note)
+  """.format(transpose=note_to_decimal(note), instrument=instrument - 1))
 
 #
 # convert an abc score to an mp3 file
 #
-def make_mp3(score, transpose=0, tempo_percent=100, name=""):
+def make_mp3(score, transpose=0, tempo_percent=100, filename=""):
   if MAKE_MP3S:
     key = str(locals())
     if key not in mp3s:
       mp3s.add(key)
-      outfile = goaldir + "/" + drillnum() + "B." + (name if name else str(tempo_percent)) + ".mp3"
+      outfile = goaldir + "/" + (filename if filename else drillnum() + "B.mp3")
       os.system("""echo '{score}' \
           | abc2midi /dev/stdin -o /dev/stdout \
           | timidity - --quiet --quiet --output-24bit -A800 -K{transpose} -T{tempo_percent} -Ow -o - \
@@ -95,7 +94,7 @@ def make_chunk(start_secs, stop_secs, tempo, padding=2.5, silence=5):
     to = stop_secs + padding
     st = stop_secs - start_secs + padding
     tempo_mult = tempo / goaltempo
-    outfile = goaldir + "/" + drillnum() + "C.mp3"
+    outfile = goaldir + "/" + drillnum() + "B.mp3"
     os.system("""
     ffmpeg -nostdin -loglevel error -ss {ss} -to {to} -i {filename} -ac 1 -ar 48000 -q 4 \
            -af afade=d={padding},afade=t=out:st={st}:d={padding},atempo={tempo_mult},adelay={silence}s:all=true \
