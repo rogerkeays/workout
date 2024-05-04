aa7b747e786b24bf7419a70f5928ba38153ab169
Roger Keays <guybrush@mail8.simplelogin.com>
2024-05-04 21:23:15 +0700

    lightly row: add missing phrase

--- violin_scores.py
+++ violin_scores.py
@@ -53,6 +53,7 @@ def lightly_row(tempo=90):
     phrase(tempo, "always rowing, never slowing", "QQQQQQQQ", "12222222", "NWWWWWWW", "2", "02223111", "UD", start=14.12, stop=17.60),
     phrase(tempo, "in my bright new red canoe", "QQQQQQH", "2211222", "WWNNWWW", "2", "0200222", "UD", start=17.60, stop=21.23),
     phrase(tempo, "see the fishes swimming by", "QQQQQQH", "2", "W", "2", "1111123", start=21.23, stop=24.85),
+    phrase(tempo, "see the birds up in the sky", "QQQQQQH", "2", "W", "2", "2222234", "UD", start=24.85, stop=28.58),
   )
 
 def ponies(tempo=95):
