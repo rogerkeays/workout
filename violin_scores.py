31b492037f749d9b5239d77e7547c941fd501d6a
Roger Keays <guybrush@mail8.simplelogin.com>
2024-05-27 19:28:17 +0700

    remove repeats in scores

--- violin_scores.py
+++ violin_scores.py
@@ -16,60 +16,60 @@ def warmup():
 
 def the_crawl(tempo=90):
   score("the-crawl", locate("01.the-crawl.mp3"), [ 10.16, 15.38, 21.53 ], tempo,
-    lyrics  = "| >li-ttle ba- by | crawls to dan-ger | >scared he turns round | in a cir-cle",
-    rhythm  = "|  Q   Q   Q   Q  |    Q   Q   Q   Q  |    Q    Q    Q    Q    | Q  Q  Q  Q  ",
-    strings = "|  4   4   3   3  |    2   2   1   1  |    2    2    3    3    | 2  2  3  3  ",
+    lyrics  = "| >li-ttle ba- by | crawls to dan-ger | >scared he turns round | in a cir-cle ",
+    rhythm  = "|  Q   =   =   =  |    =   =   =   =  |    =    =    =    =    | =  =  =  =   ",
+    strings = "|  4   =   3   =  |    2   =   1   =  |    2    =    3    =    | 2  =  3  =   ",
     shapes  = "|  N "
   )
 
 def baby_steps(tempo=90):
   score("baby-steps", locate("02.baby-steps.mp3"), [ 10.30, 13.05, 15.67, 18.30, 21.75 ], tempo,
-    lyrics  = "| >left foot step | >right foot step | >looks for mu-mmy  | >fall-ing down",
-    rhythm  = "|   Q    Q    H   |    Q    Q    H   |    Q   Q   Q   Q   |    Q   Q   H  ",
-    strings = "|   4    4    3   |    2    2    3   |    2   2   1   1   |    2   3   4  ",
-    bowing  = "|  35    3    5   |    3    5    3   |    5   3   5   3   |    5   3   5  ",
+    lyrics  = "| >left foot step | >right foot step | >looks for mu-mmy  | >fall-ing down ",
+    rhythm  = "|   Q    =    H   |    Q    =    H   |    Q   =   =   =   |    =   =   H   ",
+    strings = "|   4    =    3   |    2    =    3   |    2   =   1   =   |    2   3   4   ",
+    bowing  = "|  35    3    5   |    3    5    3   |    5   3   5   3   |    5   3   5   ",
     shapes  = "|   N"
   )
 
 def the_car_trip(tempo=110):
   score("the-car-trip", locate("03.the-car-trip.mp3"), [ 9.72, 12.04, 14.22, 16.52, 19.46 ], tempo,
-    lyrics  = "| >li-ttle ba- by | throw-ing up | >mu-mmy wants to | throw up too | >pull-ing o- ver | >clean it up",
-    rhythm  = "|  Q   Q   Q   Q  |   Q   Q   H  |   Q   Q   Q   Q  |   Q   Q   H  |   Q   Q   Q   Q  |   Q   Q   H ",
-    strings = "|  1   1   2   2  |   3   3   2  |   3   3   4   4  |   3   3   2  |   1   2   3   4  |   3   3   2 ",
-    bowing  = "| 35   3   5   3  |   5   3   5  |   3   5   3   5  |   3   5   3  |   5   3   5   3  |   5   3   5 ",
+    lyrics  = "| >li-ttle ba- by | throw-ing up | >mu-mmy wants to | throw up too | >pull-ing o- ver | >clean it up ",
+    rhythm  = "|  Q   =   =   =  |   =   =   H  |   Q   =   =   =  |   =   =   H  |   Q   =   =   =  |   =   =   H  ",
+    strings = "|  1   =   2   =  |   3   =   2  |   3   =   4   =  |   3   =   2  |   1   2   3   4  |   3   =   2  ",
+    bowing  = "| 35   3   5   3  |   5   3   5  |   3   5   3   5  |   3   5   3  |   5   3   5   3  |   5   3   5  ",
     shapes  = "|  N"
   )
 
 def aeroplane_games(tempo=110):
   score("aeroplane-games", locate("04.aeroplane-games.mp3"), [ 9.59, 10.82, 12.06, 14.24, 17.01 ], tempo,
     lyrics  = "| >fly-ing up-wards >fly-ing down-wards | >>land-ing at the term-in-al ",
-    rhythm  = "|   E   E   E   E      E   E   E   E    |    E   E   E   E   E   E   Q ",
-    strings = "|   4   3   2   1      1   2   3   4    |    3   3   2   2   1   1   2 ",
+    rhythm  = "|   E   =   =   =      =   =   =   =    |    =   =   =   =   =   =   Q ",
+    strings = "|   4   3   2   1      1   2   3   4    |    3   =   2   =   1   =   2 ",
     shapes  = "|   N"
   )
 
 def lightly_row(tempo=90):
   lyrics   = "| >light-ly row | fla-min- go | >down the ri-ver | we will go "
-  rhythm   = "|   Q   Q   H   |  Q   Q   H  |   Q   Q   Q   Q  |  Q   Q   H "
-  strings  = "|   1   2   2   |  2   2   2  |   2   2   2   2  |  1   1   1 "
+  rhythm   = "|   Q   =   H   |  Q   =   H  |   Q   =   =   =  |  =   =   H "
+  strings  = "|   1   2   =   |  =   =   =  |   =   =   =   =  |  1   =   = "
   bowing   = "|  35   3   5   |  3   5   3  |   5   3   5   3  |  5   3   5 "
-  shapes   = "|   N   W   W   |  W   W   W  |   W   W   W   W  |  N   N   N "
-  fingers  = "|   0   2   2   |  3   1   1  |   0   1   2   3  |  0   0   0 "
+  shapes   = "|   N   W   =   |  =   =   =  |   =   =   =   =  |  N   =   = "
+  fingers  = "|   0   2   =   |  3   1   =  |   0   1   2   3  |  0   =   = "
   bases    = "|   2"
 
-  lyrics  += "| >al-ways row-ing | ne-ver slow-ing | >in my bright big | red ca- noe"
-  rhythm  += "|   Q   Q   Q   Q  |  Q   Q   Q   Q  |   Q   Q   Q   Q   |  Q   Q   H "
-  strings += "|   1   2   2   2  |  2   2   2   2  |   2   2   1   1   |  2   2   2 "
-  bowing  += "|   3   5   3   5  |  3   5   3   5  |   3   5   3   5   |  3   5   3 "
-  shapes  += "|   N   W   W   W  |  W   W   W   W  |   W   W   N   N   |  W   W   W "
-  fingers += "|   0   2   2   2  |  3   1   1   1  |   0   2   0   0   |  2   2   2 "
+  lyrics  += "| >al-ways row-ing | ne-ver slow-ing | >in my bright big | red ca- noe "
+  rhythm  += "|   Q   =   =   =  |  =   =   =   =  |   =   =   =   =   |  =   =   H  "
+  strings += "|   1   2   =   =  |  =   =   =   =  |   =   =   1   =   |  =   =   =  "
+  bowing  += "|   3   5   3   5  |  3   5   3   5  |   3   5   3   5   |  3   5   3  "
+  shapes  += "|   N   W   =   =  |  =   =   =   =  |   =   =   N   =   |  W   =   =  "
+  fingers += "|   0   2   2   2  |  3   1   1   1  |   0   2   0   0   |  2   2   2  "
 
-  lyrics  += "| >see the fish-es | swim-ming by | >see the birds up | in  the sky"
-  rhythm  += "|   Q   Q   Q   Q  |   Q   Q   H  |   Q   Q   Q   Q   |  Q   Q   H "
-  strings += "|   2   2   2   2  |   2   2   2  |   2   2   2   2   |  2   2   2 "
-  bowing  += "|   5   3   5   3  |   5   3   5  |   3   5   3   5   |  3   5   3 "
-  shapes  += "|   W   W   W   W  |   W   W   W  |   W   W   W   W   |  W   W   W "
-  fingers += "|   1   1   1   1  |   1   2   3  |   2   2   2   2   |  2   3   4 "
+  lyrics  += "| >see the fish-es | swim-ming by | >see the birds up | in  the sky "
+  rhythm  += "|   Q   =   =   =  |   =   =   H  |   Q   =   =   =   |  =   =   H  "
+  strings += "|   2   =   =   =  |   =   =   =  |   =   =   =   =   |  =   =   =  "
+  bowing  += "|   5   3   5   3  |   5   3   5  |   3   5   3   5   |  3   5   3  "
+  shapes  += "|   W   =   =   =  |   =   =   =  |   =   =   =   =   |  =   =   =  "
+  fingers += "|   1   =   =   =  |   =   2   3  |   2   =   =   =   |  =   3   4  "
 
   score("lightly-row", locate("27.lightly-row.mp3"), [ 6.92, 10.52, 14.12, 17.60, 21.23, 24.85, 28.58 ], 
         tempo, lyrics, rhythm, strings, bowing, shapes, bases, fingers)
@@ -118,5 +118,5 @@ def musette(tempo=120):
   attack  += "|    V   =   S   V   =   S  | S   =   V |    S    =    V    =    =    = |  =  "
 
   score("musette", locate("35.musette.mp3"), [ 9.77, 14.07, 17.87, 21.93, 25.84, 29.69, 33.59, 37.43, 41.55 ],
-        tempo, lyrics, rhythm, strings, bowing, shapes, bases, fingers)
+        tempo, lyrics, rhythm, strings, bowing, shapes, bases, fingers, "", attack)
 
