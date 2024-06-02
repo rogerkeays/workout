2ab48b013105830b6063a8adba8d906dc0083a4b
Roger Keays <guybrush@mail8.simplelogin.com>
2024-06-02 18:44:22 +0700

    patch all of me lyrics and rhythm

--- violin_scores.py
+++ violin_scores.py
@@ -115,48 +115,48 @@ def musette(tempo=120):
     attack  = "|D====|=====|==SSS|D===|==SSS|D==R|D=SD=S|==D|==SD=S|==D|S=D===|="
   )
 
-def all_of_me(tempo=126):
-  phrases  = [("what would i do with-out your smart mouth? .", 23.51),
-              ("draw-in me in a you kick-in me a-out .", 27.31),
-              ("you've got my head spin-nin , no kid-din .", 31.12),
-              ("i can't , pin yo-u down .", 35.85)]
+def all_of_me(tempo=126, tonic=48, meter=4):
+  phrases  = [("what would i do with-o-ut your smart mouth?", 23.51),
+              ("draw-in me in a you kick-in me a-out", 27.31),
+              ("you've got my head spin-nin , no kid-din", 31.12),
+              ("i can't , pin yo-u down", 35.85)]
 
-  index    = "wwi|dwoys|m. dim|iaykima|o. ygm|hsn,|nkd. i|c,pyu|d. "
-  rhythm   = "3e4|1a234|12 3e4|1a23e4o|12 u3t|13e4|13e4 o|1u3e4|12 "
-  degrees  = "24=|====0|0. 24=|=24=20=|9. 94=|540,|540. 0|2,429|=. "
-  strings  = "3==|=====|=. ===|=======|=. ===|===,|===. =|=,===|=. "
-  bases    = "6==|=====|=. ===|=======|=. ===|===,|===. =|=,===|=. "
-  bowing   ="3535|43535|3. 535|4354353|5. 353|535,|354. 3|5,353|53 "
+  index    = "wwi|dwouys|m. dim|iaykima|o. ygm|hsn,|nkd. i|c,pyu|d. "
+  rhythm   = "3e4|1a2u34|12 3e4|1a23e4o|12 u3t|13e4|13e4 o|1u3eo|1u "
+  degrees  = "24=|===?=0|0. 24=|=24=20=|9. 94=|540,|540. 0|2,429|=. "
+  strings  = "3==|======|=. ===|=======|=. ===|===,|===. =|=,===|=. "
+  bases    = "6==|======|=. ===|=======|=. ===|===,|===. =|=,===|=. "
+  bowing   ="3535|435435|3. 535|4354353|5. 353|535,|354. 3|5,353|53 "
 
-  phrases += [("what's go-in on in that beau-ti-ful mind? .", 38.95),
-              ("i'm on your ma-gi-cal my-ste-ry r-ide .", 42.67),
-              ("and i'm so di-zzy , don't kn-ow what hit me .", 46.57),
-              ("but i'll , be a-l-right .", 51.03)]
+  phrases += [("what's go-in on in that beau-ti-ful mind?", 38.95),
+              ("i'm on your ma-gi-cal my-ste-ry r-ide", 42.67),
+              ("and i-m so di-zzy , don't kn-ow what hit me", 46.57),
+              ("but i'll be a-l-right", 51.03)]
 
-  index   += "wgi|oitbtfm|. ioy|mgcmsrr|i. ai|sdz,d|kowhm. b|i,bal|r. "
-  rhythm  += "3e4|1a23e4o|1 3e4|1a23e4o|12 3e|13e4o|a?23e4 o|1u3e4|12 "
-  degrees += "24=|=24=20=|. 4==|754=20=|9. 4=|540,=|45=40. 0|2,42?|5. "
-  strings += "3==|=======|. ===|=======|=. ==|===,=|=====. =|=,===|=. "
-  bases   += "6==|=======|. ===|=======|=. ==|===,=|=====. =|=,===|=. "
-  bowing  += "535|4354353|. 535|3534535|3. 53|535,3|54354. 3|5,432|5. "
+  index   += "wgi|oitbtfm|. ioy|mgcmsrr|i. aim|sdz,d|kowhm. b|ibal|r. "
+  rhythm  += "3e4|1a23e4w|2 3e4|1a23e4o|1u 3em|13e4o|1b23e4 o|13eo|1e "
+  degrees += "24=|=24=20=|. 4==|754=20=|9. 4==|540,=|45=40. 0|242?|5. "
+  strings += "3==|=======|. ===|=======|=. ===|===,=|=====. =|====|=. "
+  bases   += "6==|=======|. ===|=======|=. ===|===,=|=====. =|====|=. "
+  bowing  += "535|4354353|. 535|3534535|3. 534|535,3|54354. 3|5432|5. "
 
   phrases += [("my h-ead's u-n-de-r-wa-te-r but", 54.92),
               ("i'm br-ea-th-in fine", 58.71),
-              ("you're cr-a-zy and i'm o-ut of my mind", 62.57)]
+              ("y-ou're c-ra-zy and i'm o-ut of my mind", 62.57)]
 
-  index   += "m|heundrw|trb. i|betif|. y|cazai|otomm|. "
-  rhythm  += "o|1bu3e4o|u3e4 o|u3e4o|? o|u3e4o|u3e4o|1 "
-  degrees += "0|7975=4=|20=. Y|97=9=|. 9|=7=54|=2=02|. "
+  index   += "m|heundrw|trb. i|betif|. yo|crzai|otomm|. "
+  rhythm  += "o|1bu3e4o|u3e4 o|u3e4o|u ow|u3e4o|u3e4o|3 "
+  degrees += "0|7975=4=|20=. Y|97=9=|. ?9|=7=54|=2=02|. "
   bowing  += "3"
 
   phrases += [("cause a-a-ll of m-e", 68.21),
               ("loves all of y-ou", 71.10),
-              ("love your curves and all your ed-ges", 74.86),
+              ("love you-r curves and all your ed-ges", 74.86),
               ("all you-r per-fect im-per-fec-tions", 78.86)]
 
-  index   += "c|aalom|e. l|aoy|u. ly|caaye|g. ayr|pfipf|t. "
-  rhythm  += "o|1??4o|1? 3|14o|1? 3e|1234o|1? 3eo|1234o|1? "
-  degrees += "4|7??47|9. 4|202|4. 4=|2==02|9. 4=2|===02|9. "
+  index   += "c|aalom|e. l|aoy|u. lyr|caaye|g. ayr|pfipf|t. "
+  rhythm  += "o|1b34o|12 3|14o|12 3eo|1234o|12 3eo|1234o|12 "
+  degrees += "4|7??47|9. 4|202|4. 4=2|===02|9. 4=2|===02|9. "
 
   phrases += [("give your a-ll to m-e", 82.63),
               ("i'll give m-y all to yo-u", 86.35),
@@ -164,52 +164,53 @@ def all_of_me(tempo=126):
               ("e-ven when i lose i'm win-ning", 94.17)]
 
   index   += "gy|altm|e. igmy|aty|u. ymy|eambg|n. ev|wiliw|n. "
-  rhythm  += "3e|1b4o|1? u3e?|14o|1? 3eo|1234o|1? 3e|1234o|1? "
+  rhythm  += "3e|1b4o|12 u3eo|14o|12 3eo|1234o|1a 3e|1234o|1a "
   degrees += "4=|=749|L. 294?|202|4. 4=2|===02|0. 4=|2==02|9. "
 
-  phrases += [("cause i give you a-uau-a-ll of me", 97.70),
-              ("and you give me a-uau-a-ll of you , oh-o", 105.28)]
+  phrases += [("cause i give you a-u-i-a-a-ll , of me", 97.70),
+              ("and you give me a-u-i-a-a-ll , of you", 105.28),
+              ("oh-o", 111.83)]
 
-  index   += "cigya|ua|lom. aygma|ua|loy|,oo|. "
-  rhythm  += "u3e4o|uo|u4o? u3e4o|uo|u4o|?4o|? "
-  degrees += "4=570|L9|74=. 4=570|L9|74=|,=2|. "
+  index   += "cigya|uiaa|l,om|. |aygma|uiaa|l,oy|. oo|. "
+  rhythm  += "u3e4o|up3o|u34o|u |u3e4o|up3o|u34o|u 4o|3 "
+  degrees += "4=570|L??9|7,4=|. |4=570|L??9|7,4=|. =2|. "
 
-  phrases += [("how ma-ny times do i have to tell you", 115.05),
+  phrases += [("how ma-n-y t-imes do i have to tell you", 115.05),
               ("e-ven when you're c-ry-ing you're beau-ti-ful t-oo", 118.98),
-              ("the world is bea-ting you down i'm a-round", 122.55),
+              ("the w-orld is bea-t-ing you down i-m a-round", 122.55),
               ("through e-ve-ry mood", 128.17)]
 
-  index   += "hmn|tdihtt|y. evwy|criybtft|o. twi|btyd|iar|. tevrm|. "
-  rhythm  += "3e4|12u3e4|1? 3e4o|1b2u3e4o|1? u3t|1ueo|u4o|? u3e4o|? "
-  degrees += "24=|==2420|=. 4===|754=20=9|?. 04=|5405|402|. 242=0|. "
+  index   += "hmny|tidihtt|y. evwy|criybtft|o. twoi|btiyd|imar|. tevrm|. "
+  rhythm  += "3e4m|1a2u3e4|12 3e4o|1b2u3e4o|12 u3ct|1u3eo|ue4o|2 u3e4o|2 "
+  degrees += "24=?|=?=2420|=. 4===|754=20=9|?. 0?4=|540=5|40=2|. 242=0|. "
 
-  phrases += [("you're my do-wn-fall, you're my mu-se", 130.19),
-              ("my w-orst dis-tr-ac-tion , my rhy-thm n blues", 133.97),
-              ("i can't stop si-ng-ing, it's ri-ng-ing in", 137.69),
-              ("my h-ead for y-ou", 142.49, 150.28)]
+  phrases += [("you're my down-fall, you're my muse", 130.19),
+              ("my w-orst dis-tr-ac-tion, my rhy-thm n blues", 133.97),
+              ("i can't stop si-ng-ing it's , r-i-ng-ing in", 137.69),
+              ("my , h-ead for y-ou", 142.49, 150.28)]
 
-  index   += "ymd|wfymm|s. mwodt|at,mrtnb. ics|sniir|nii. m|hefy|o. "
-  rhythm  += "peo|1a3eo|1? u3e4o|1a?u3e4o? u3t|1u3eo|u3e? o|ue4o|1? "
-  degrees += "74=|0=744|?. 0574=|2=,=4249. 44=|540=5|40=. 2|42=4|5. "
+  index   += "ymd|fymm|. mwodt|atmrtnb|. ics|snii,r|inii. m|,hefy|o. "
+  rhythm  += "peo|a3eo|a u3e4o|1au3e4o|2 u3t|1u3e4o|1u3e4 o|2ue4o|14 "
+  degrees += "74=|0744|. 0574=|2==4249|. 44=|540=,4|540=. 2|,42=4|5. "
 
-  phrases += [("i'll give m-y all to y-ou", 162.63, 166.38)]
+  phrases += [("i'll give m-y all to you", 178.75, 181.63)]
 
-  index   += "igmy|aty|u. "
-  rhythm  += "u3e?|14o|1? "
-  degrees += "40Y9|=74|?. "
+  index   += "igmy|aty|. "
+  rhythm  += "u3eo|14o|2 "
+  degrees += "40Y9|=74|. "
 
-  phrases += [("give me all of you o-o-o-o-oh", 204.06),
+  phrases += [("give me a-ll of you o-u-o-u-oh", 204.06),
               ("cards o-n the t-a-b-le, w-e're both sh-o-w-ing hearts", 207.59),
               ("ris-king it a-ll, th-ough it's hard", 215.01, 220.54)]
 
-  index   += "gmaoy|ooooo. |contt|ablweb|sowih|. rkia|ltoih. "
-  rhythm  += "a2ue4|up3ce? |1u3eo|1u3e4o|u3e4o|? 1u3o|1u3eo? "
-  degrees += "570Y9|75420. |975=Y|0Y9=7=|54=2=|. 975Y|0Y9=7. "
+  index   += "gmaloy|ououo. |contt|ablweb|sowih|. |rkia|ltoih|. "
+  rhythm  += "a2u3e4|up3ce1 |1u3eo|1u3e4o|u3e4o|e |1u3o|1u3eo|3 "
+  degrees += "570Y=9|75420. |975=Y|0Y9=7=|54=2=|. |975Y|0Y9=7|. "
 
-  phrases += [("i give you a-u-a-u-a-ll of m-e", 265.26),
-              ("and you give me a-uau-a-ll of you, o-u-oh", 273.04, 282.03)]
+  phrases += [("i give you a-u-a-u-a-ll , of me", 265.26),
+              ("o-u-oh", 279.42, 282.03)]
 
-  index   += "igya|uaua|lome. aygma|ua|loy|,ou|o. "
-  rhythm  += "3e4o|?u?o|u4o?? u3e4o|uo|u4o|?4o|1? "
-  degrees += "4570|2L09|74=?. 4=570|L9|74=|,24|2. "
+  index   += "igya|uaua|l,om|. ou|o. "
+  rhythm  += "3e4o|2u4o|u34o|3 4o|14 "
+  degrees += "4570|2L09|7,4=|. 24|2. "
 
