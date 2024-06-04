87ec282e55c26239cc21c42e1a89396c95b20639
Roger Keays <guybrush@mail8.simplelogin.com>
2024-06-04 15:45:17 +0700

    patch all of me melody

--- violin_scores.py
+++ violin_scores.py
@@ -123,7 +123,7 @@ def all_of_me(tempo=126, tonic=48, meter=4):
 
   index    = "wwi|dwouys|m. dim|iaykima|o. ygm|hsn,|nkd. i|c,pyu|d. "
   rhythm   = "3e4|1a2u34|12 3e4|1a23e4o|12 u3t|13e4|13e4 o|1u3eo|1u "
-  degrees  = "24=|===?=0|0. 24=|=24=20=|9. 94=|540,|540. 0|2,429|=. "
+  degrees  = "24=|==24=0|0. 24=|=24=20=|9. 94=|540,|540. 0|2,429|=. "
   strings  = "3==|======|=. ===|=======|=. ===|===,|===. =|=,===|=. "
   bases    = "6==|======|=. ===|=======|=. ===|===,|===. =|=,===|=. "
   bowing   ="3535|435435|3. 535|4354353|5. 353|535,|354. 3|5,353|53 "
@@ -135,18 +135,18 @@ def all_of_me(tempo=126, tonic=48, meter=4):
 
   index   += "wgi|oitbtfm|. ioy|mgcmsrr|i. aim|sdz,d|kowhm. b|ibal|r. "
   rhythm  += "3e4|1a23e4w|2 3e4|1a23e4o|1u 3em|13e4o|1b23e4 o|13eo|1e "
-  degrees += "24=|=24=20=|. 4==|754=20=|9. 4==|540,=|45=40. 0|242?|5. "
+  degrees += "24=|=24=20=|. 4==|754=20=|9. 4==|540,=|45=40. 0|242=|5. "
   strings += "3==|=======|. ===|=======|=. ===|===,=|=====. =|====|=. "
   bases   += "6==|=======|. ===|=======|=. ===|===,=|=====. =|====|=. "
   bowing  += "535|4354353|. 535|3534535|3. 534|535,3|54354. 3|5432|5. "
 
   phrases += [("my h-ead's u-n-de-r-wa-te-r but", 54.92),
               ("i'm br-ea-th-in fine", 58.71),
-              ("y-ou're c-ra-zy and i'm o-ut of my mind", 62.57)]
+              ("y-ou're cr-a-zy and i'm o-ut of my mind", 62.57)]
 
-  index   += "m|heundrw|trb. i|betif|. yo|crzai|otomm|. "
+  index   += "m|heundrw|trb. i|betif|. yo|cazai|otomm|. "
   rhythm  += "o|1bu3e4o|u3e4 o|u3e4o|u ow|u3e4o|u3e4o|3 "
-  degrees += "0|7975=4=|20=. Y|97=9=|. ?9|=7=54|=2=02|. "
+  degrees += "0|7975=4=|20=. Y|97=9=|. 79|=7=54|=2=02|. "
   bowing  += "3"
 
   phrases += [("cause a-a-ll of m-e", 68.21),
@@ -156,16 +156,16 @@ def all_of_me(tempo=126, tonic=48, meter=4):
 
   index   += "c|aalom|e. l|aoy|u. lyr|caaye|g. ayr|pfipf|t. "
   rhythm  += "o|1b34o|12 3|14o|12 3eo|1234o|12 3eo|1234o|12 "
-  degrees += "4|7??47|9. 4|202|4. 4=2|===02|9. 4=2|===02|9. "
+  degrees += "4|=7=47|9. 4|202|4. 4=2|===02|9. 4=2|===02|9. "
 
   phrases += [("give your a-ll to m-e", 82.63),
               ("i'll give m-y all to yo-u", 86.35),
               ("you're m-y end and my be-gin-ning", 90.10),
-              ("e-ven when i lose i'm win-ning", 94.17)]
+              ("e-ve-n when i lose i'm win-ning", 94.17)]
 
-  index   += "gy|altm|e. igmy|aty|u. ymy|eambg|n. ev|wiliw|n. "
-  rhythm  += "3e|1b4o|12 u3eo|14o|12 3eo|1234o|1a 3e|1234o|1a "
-  degrees += "4=|=749|L. 294?|202|4. 4=2|===02|0. 4=|2==02|9. "
+  index   += "gy|altm|e. igmy|aty|u. ymy|eambg|n. evn|wiliw|n. "
+  rhythm  += "3e|1b4o|12 u3eo|14o|12 3eo|1234o|1a 3eo|1234o|1a "
+  degrees += "4=|=747|9. 2942|=02|4. 4=2|===02|9. 4=2|===02|9. "
 
   phrases += [("cause i give you a-u-i-a-a-ll , of me", 97.70),
               ("and you give me a-u-i-a-a-ll , of you", 105.28),
@@ -173,16 +173,16 @@ def all_of_me(tempo=126, tonic=48, meter=4):
 
   index   += "cigya|uiaa|l,om|. |aygma|uiaa|l,oy|. oo|. "
   rhythm  += "u3e4o|up3o|u34o|u |u3e4o|up3o|u34o|u 4o|3 "
-  degrees += "4=570|L??9|7,4=|. |4=570|L??9|7,4=|. =2|. "
+  degrees += "4=570|Y0Y9|7,4=|. |4=570|Y0Y9|7,4=|. =2|. "
 
   phrases += [("how ma-n-y t-imes do i have to tell you", 115.05),
               ("e-ven when you're c-ry-ing you're beau-ti-ful t-oo", 118.98),
-              ("the w-orld is bea-t-ing you down i-m a-round", 122.55),
+              ("the w-orld is bea-t-ing you d-own i-m a-round", 122.55),
               ("through e-ve-ry mood", 128.17)]
 
-  index   += "hmny|tidihtt|y. evwy|criybtft|o. twoi|btiyd|imar|. tevrm|. "
-  rhythm  += "3e4m|1a2u3e4|12 3e4o|1b2u3e4o|12 u3ct|1u3eo|ue4o|2 u3e4o|2 "
-  degrees += "24=?|=?=2420|=. 4===|754=20=9|?. 0?4=|540=5|40=2|. 242=0|. "
+  index   += "hmny|tidihtt|y. evwy|criybtft|o. twoi|btinyd|oimar|. tevrm|. "
+  rhythm  += "3e4m|1a2u3e4|12 3e4o|1b2u3e4o|12 u3ct|1up3eo|1ue4o|2 u3e4o|2 "
+  degrees += "2424|24=2420|=. 4===|5754=20=|9. 024=|545404|540=2|. 242=9|. "
 
   phrases += [("you're my down-fall, you're my muse", 130.19),
               ("my w-orst dis-tr-ac-tion, my rhy-thm n blues", 133.97),
@@ -205,7 +205,7 @@ def all_of_me(tempo=126, tonic=48, meter=4):
 
   index   += "gmaloy|ououo. |contt|ablweb|sowih|. |rkia|ltoih|. "
   rhythm  += "a2u3e4|up3ce1 |1u3eo|1u3e4o|u3e4o|e |1u3o|1u3eo|3 "
-  degrees += "570Y=9|75420. |975=Y|0Y9=7=|54=2=|. |975Y|0Y9=7|. "
+  degrees += "57Y0Y9|75420. |975=Y|0Y9=7=|54=2=|. |975Y|0Y9=7|. "
 
   phrases += [("i give you a-u-a-u-a-ll , of me", 265.26),
               ("o-u-oh", 279.42, 282.03)]
