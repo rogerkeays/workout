157920f06197d21fa7faf3b5607dc95230103986
Roger Keays <guybrush@mail8.simplelogin.com>
2025-11-14 17:53:00 +0800

    convert old horizontal scores to latest syntax: alison's etudes

--- scores/alison.py
+++ scores/alison.py
@@ -5,64 +5,125 @@ from violin import *
 
 process_piece(Piece("the-crawl", 4, 90, "37", [
   Section("table", "V", [
-    Phrase("crawls", parse_violin_notes("""
-      1= 0 04N0 3v=LM li-
-      2= = ==== 5^=== ttle
-      3= 7 =3== 3v=== ba-
-      4= = ==== 5^=== by
-      1= 2 =2== 3v=== crawls
-      2= = ==== 5^=== to
-      3= 9 =1== 3v=== dan-
-      41 = ==== 5^3== ger"""), 10.16, 15.38
+    Phrase("crawls", notes("""
+      1 0 L4== 40N0 3 li-
+      2 = ==== ==== 5 ttle
+      3 7 ==== 3=== 3 ba-
+      4 = ==== ==== 5 by
+      1 2 ==== 2=== 3 crawls
+      2 = ==== ==== 5 to
+      3 9 ==== 1=== 3 dan-
+      4 = ==== ==== 5 ger
+      1 Z ==== ==== 3 ."""), 10.16, 15.38
     ),
-    Phrase("scared", parse_violin_notes("""
-      1= 2 02N0 3v=LM scared
-      2= = ==== 5^=== he
-      3= 7 =3== 3v=== turns
-      4= = ==== 5^=== round
-      1= 2 =2== 3v=== in
-      2= = ==== 5^=== a
-      3= 7 =3== 3v=== cir-
-      41 = ==== 5^3== cle"""), 15.38, 21.53
+    Phrase("scared", notes("""
+      1 2 L4== 20N0 3 scared
+      2 = ==== ==== 5 he
+      3 7 ==== 3=== 3 turns
+      4 = ==== ==== 5 round
+      1 2 ==== 2=== 3 in
+      2 = ==== ==== 5 a
+      3 7 ==== 3=== 3 cir-
+      4 = ==== ==== 5 cle
+      1 Z ==== ==== 3 ."""), 15.38, 21.53
     ),
   ])
 ], "01.the-crawl.mp3"))
 
-def baby_steps(meter=4, tempo=90, tonic=37):
-  piece(2, "baby-steps", locate("02.baby-steps.mp3"), tempo,
-    phrases = [("left foot step", 10.30),
-               ("right foot step", 13.05),
-               ("looks for mu-mmy", 15.67),
-               ("fall-ing down", 18.30, 21.75)],
+process_piece(Piece("baby-steps", 4, 90, "37", [
+  Section("lawn", "V", [
+    Phrase("left", notes("""
+      1 0 L4== 40N0 3 left
+      2 = ==== ==== 5 foot
+      3 7 ==== 3=== 3 step
+      1 Z ==== ==== 5 ."""), 10.30, 13.05
+    ),
+    Phrase("right", notes("""
+      1 2 L4== 20N0 5 right
+      2 = ==== ==== 3 foot
+      3 0 ==== 3=== 5 step
+      1 Z ==== ==== 3 ."""), 13.05, 15.67
+    ),
+    Phrase("mummy", notes("""
+      1 2 L4== 20N0 3 looks
+      2 = ==== ==== 5 for
+      3 7 ==== 1=== 3 mu-
+      4 = ==== ==== 5 mmy
+      1 Z ==== ==== 3 ."""), 15.67, 18.30
+    ),
+    Phrase("falling", notes("""
+      1 2 L4== 20N0 3 fall-
+      2 = ==== 3=== 5 ing
+      3 7 ==== 4=== 3 down
+      1 Z ==== ==== 5 ."""), 18.30, 21.75
+    ),
+  ])
+], "02.baby-steps.mp3"))
 
-    index   = "|lfs |rfs |lfmm |fid",
-    rhythm  = "|123 |123 |1234 |123",
-    strings = "|4=3 |2=3 |2=1= |234",
-    shapes  = "|N== |=== |==== |==="
-  )
+process_piece(Piece("the-car-trip", 4, 110, "37", [
+  Section("car", "V", [
+    Phrase("baby", notes("""
+      1 9 L4== 10N0 3 li-
+      2 = ==== ==== 5 ttle
+      3 2 ==== 2=== 3 ba-
+      4 = ==== ==== 5 by
+      1 7 ==== 3=== 3 throw-
+      2 = ==== ==== 5 ing
+      3 2 ==== 2=== 3 up
+      1 Z ==== ==== 5 ."""), 9.72, 12.04
+    ),
+    Phrase("mummy", notes("""
+      1 7 L4== 20N0 5 mu-
+      2 = ==== ==== 3 mmy
+      3 0 ==== 4=== 5 wants
+      4 = ==== ==== 3 to
+      1 7 ==== 3=== 5 throw
+      2 = ==== ==== 3 up
+      3 2 ==== 2=== 5 too
+      1 Z ==== ==== 3 ."""), 12.04, 14.22
+    ),
+    Phrase("pull", notes("""
+      1 9 L4== 10N0 3 pull-
+      2 2 ==== 2=== 5 ling
+      3 7 ==== 3=== 3 o
+      4 0 ==== 4=== 5 ver
+      1 Z ==== ==== 3 ."""), 14.22, 16.52
+    ),
+    Phrase("clean", notes("""
+      1 7 L4== 20N0 3 clean
+      2 = ==== ==== 5 it
+      3 2 ==== 3=== 3 up
+      1 Z ==== ==== 5 ."""), 16.52, 19.46
+    ),
+  ])
+], "03.the-car-trip.mp3"))
 
-def the_car_trip(meter=4, tempo=110, tonic=37):
-  piece(3, "the-car-trip", locate("03.the-car-trip.mp3"), tempo,
-    phrases = [("li-ttle ba-by throw-ing up", 9.72),
-               ("mu-mmy wants to throw up too", 12.04),
-               ("pull-ing o-ver", 14.22),
-               ("clean it up", 16.52, 19.46)],
-
-    index   = "|ltbb|tiu |mmwt|tut |plov|ciu",
-    rhythm  = "|1234|123 |1234|123 |1234|123",
-    strings = "|1=2=|3=2 |3=4=|3=2 |1234|3=2",
-    shapes  = "|N===|=== |====|=== |====|==="
-  )
-
-def aeroplane_games(meter=4, tempo=110, tonic=37):
-  piece(4, "aeroplane-games", locate("04.aeroplane-games.mp3"), tempo,
-    phrases = [("fly-ing up-wards", 9.59),
-               ("fly-ing down-wards", 10.82, 12.06),
-               ("land-ing at the term-in-al", 14.24, 17.01)],
-
-    index   = "|fidw fiup |ldattmn",
-    rhythm  = "|1a2u 3e4o |1a2u3e4",
-    strings = "|4321 1234 |3=2=1=2",
-    shapes  = "|N=== ==== |======="
-  )
+process_piece(Piece("aeroplane-games", 4, 110, "37", [
+  Section("airfield", "V", [
+    Phrase("upwards", notes("""
+      1 0 L4== 40N0 3 fly-
+      a 7 ==== 3=== 5 ing
+      2 2 ==== 2=== 3 up-
+      u 9 ==== 1=== 5 wards
+      3 Z ==== ==== 3 ."""), 9.59, 10.82
+    ),
+    Phrase("downwards", notes("""
+      1 9 L4== 10N0 3 fly-
+      a 2 ==== 2=== 5 ing
+      2 7 ==== 3=== 3 down-
+      u 0 ==== 4=== 5 wards
+      3 Z ==== ==== 3 ."""), 10.82, 12.06
+    ),
+    Phrase("terminal", notes("""
+      1 7 L4== 30N0 3 land-
+      a = ==== ==== 5 ing
+      2 2 ==== 2=== 3 at
+      u = ==== ==== 5 the
+      3 9 ==== 1=== 3 ter-
+      e = ==== ==== 5 mi-
+      4 2 ==== 2=== 3 nal
+      1 Z ==== ==== 5 ."""), 14.24, 17.01
+    ),
+  ])
+], "04.aeroplane-games.mp3"))
 
