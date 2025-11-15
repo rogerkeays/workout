e62f1d7439d0b4b9494684aba0c7ca354c4b6bc9
Roger Keays <guybrush@mail8.simplelogin.com>
2025-11-15 17:24:12 +0800

    hide folding chars

--- workout.py
+++ workout.py
@@ -1,4 +1,4 @@
-# vim: foldmethod=expr foldtext=getline(v\:foldstart) foldexpr=indent(v\:lnum)\|\|indent(v\:lnum+1)\|\|getline(v\:lnum)[0]=='@'?1\:'<1'
+# vim: foldmethod=expr foldtext=getline(v\:foldstart) foldexpr=indent(v\:lnum)\|\|indent(v\:lnum+1)\|\|getline(v\:lnum)[0]=='@'?1\:'<1' fillchars=fold\:\ 
 
 # dependencies
 import sys, os, inspect, tempfile, math, shutil
