e5477a51abe7865a7f2be1ae712517bafb0cc840
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-09 22:01:36 +0700

    start piano workout code

---
+++ piano.py
@@ -0,0 +1,19 @@
+# vim: foldmethod=expr foldtext=getline(v\:foldstart) foldexpr=indent(v\:lnum)\|\|indent(v\:lnum+1)\|\|getline(v\:lnum)[0]=='@'?1\:'<1' fillchars=fold\:\ 
+
+from workout import *
+from dataclasses import dataclass
+
+# globals
+MP3_DIR = os.environ['HOME'] + "/library/workout/piano/00.source"
+
+# todo: data structures
+#@dataclass # PianoNote
+#class PianoNote(Note):
+
+# functions
+def process(piece):
+  process_all([piece])
+
+def process_all(pieces):
+  process_pieces(pieces, MP3_DIR, None, None, None, None)
+
