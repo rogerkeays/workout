73d28c25526d82a9504fad3b91f143454605906e
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-13 16:32:14 +0700

    add allegro piano score

---
+++ scores/piano/allegro.py
@@ -0,0 +1,16 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+piece(31, "allegro", 4, 135, "40", [
+  section("V", "1", [
+    phrase(10.49, "T1"),
+    phrase(14.51, "T2"),
+    phrase(18.09, "T3"),
+    phrase(21.69, "T4", stop=25.29),
+
+    phrase(10.49, "B1"),
+    phrase(14.51, "B2"),
+    phrase(18.09, "B3"),
+    phrase(21.69, "B4", stop=25.29),
+  ])
+])
+
