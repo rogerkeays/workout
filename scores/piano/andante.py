26cdd7b218bab6b13999371d159510f445f6869e
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-14 20:02:48 +0700

    patch: second phrase is different from the first

--- scores/piano/andante.py
+++ scores/piano/andante.py
@@ -3,9 +3,9 @@
 piece(32, "andante", 4, 94, "40", [
   section("A", "V1", [
     phrase(11.89, "R1"),
-    repeat(14.52, "R1"),
+    phrase(14.52, "R2"),
     repeat(17.10, "R1"),
-    phrase(19.64, "R2", stop=22.30),
+    phrase(19.64, "R3", stop=22.30),
 
     phrase(11.89, "L1"),
     phrase(14.52, "L2"),
@@ -13,10 +13,10 @@ piece(32, "andante", 4, 94, "40", [
     phrase(19.64, "L3")
   ]),
   section("B", "V2", [
-    phrase(22.30, "R3"),
-    phrase(24.85, "R4"),
-    phrase(27.37, "R5"),
-    phrase(29.89, "R6", stop=32.36),
+    phrase(22.30, "R4"),
+    phrase(24.85, "R5"),
+    phrase(27.37, "R6"),
+    phrase(29.89, "R7", stop=32.36),
 
     phrase(22.30, "L4"),
     phrase(24.85, "L5"),
