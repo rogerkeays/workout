2e06025cf45c5da18262ae4e3f71c1e182ff90c2
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-14 12:42:47 +0700

    transcribe twinkle structure and timestamps

---
+++ scores/violin/twinkle.py
@@ -0,0 +1,15 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+# @kids-bedroom
+piece(25, "twinkle", 4, 95, "49", [
+  section("A1", "window", [
+    phrase(10.20, "star"),
+    phrase(15.24, "wonder"),
+    phrase(20.27, "world"),
+    phrase(25.39, "diamond")
+  ]),
+  section("A2", "window'", [
+    repeat(30.46, "star"),
+    repeat(35.43, "wonder", 41.49)
+  ])
+])
