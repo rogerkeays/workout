e2d3bc01d4f94fd0d3c687ad88eaa3fa79047dc0
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-16 19:56:04 +0700

    treat left and right hand as single phrases

--- scores/piano/1031.study-in-g-major.py
+++ scores/piano/1031.study-in-g-major.py
@@ -1,16 +1,11 @@
 # vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
 
 piano(1031, "study-in-g-major", "pEK8h2zpaXQ", 4, 135, "40", [
-  section("V", "1", [
-    phrase(10.49, "R1"),
-    phrase(14.51, "R2"),
-    phrase(18.09, "R3"),
-    phrase(21.69, "R4", stop=25.29),
-
-    phrase(10.49, "L1"),
-    phrase(14.51, "L2"),
-    phrase(18.09, "L3"),
-    phrase(21.69, "L4", stop=25.29),
+  section("V", "V1", [
+    phrase(10.49, "P1"),
+    phrase(14.51, "P2"),
+    phrase(18.09, "P3"),
+    phrase(21.69, "P4", stop=25.29)
   ])
 ])
 
--- scores/piano/1032.study-in-c-major.py
+++ scores/piano/1032.study-in-c-major.py
@@ -2,37 +2,22 @@
 
 piano(1032, "study-in-c-major", "9T0OqxYrM-8", 4, 94, "40", [
   section("A", "V1", [
-    phrase(11.89, "R1"),
-    phrase(14.52, "R2"),
-    repeat(17.10, "R1"),
-    phrase(19.64, "R3", stop=22.30),
-
-    phrase(11.89, "L1"),
-    phrase(14.52, "L2"),
-    repeat(17.10, "L1"),
-    phrase(19.64, "L3")
+    phrase(11.89, "P1"),
+    phrase(14.52, "P2"),
+    repeat(17.10, "P1"),
+    phrase(19.64, "P3"),
   ]),
   section("B", "V2", [
-    phrase(22.30, "R4"),
-    phrase(24.85, "R5"),
-    phrase(27.37, "R6"),
-    phrase(29.89, "R7", stop=32.36),
-
-    phrase(22.30, "L4"),
-    phrase(24.85, "L5"),
-    phrase(27.37, "L6"),
-    phrase(29.89, "L7")
+    phrase(22.30, "P4"),
+    phrase(24.85, "P5"),
+    phrase(27.37, "P6"),
+    phrase(29.89, "P7"),
   ]),
   section("C", "V3", [
-    repeat(32.36, "R1"),
-    repeat(34.79, "R1"),
-    phrase(37.35, "R7"),
-    phrase(39.93, "R8", stop=43.63),
-
-    repeat(32.36, "L1"),
-    repeat(34.79, "L2"),
-    phrase(37.35, "L8"),
-    phrase(39.93, "L9", stop=43.63)
+    repeat(32.36, "P1"),
+    repeat(34.79, "P1"),
+    phrase(37.35, "P7"),
+    phrase(39.93, "P8", stop=43.63)
   ])
 ])
 
