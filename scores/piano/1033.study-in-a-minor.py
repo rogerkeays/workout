20110974c75cd283d6eab885055279723a11d0b6
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-16 19:56:25 +0700

    transcribe study in a minor structure and timestamps

---
+++ scores/piano/1033.study-in-a-minor.py
@@ -0,0 +1,11 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+piano(1033, "study-in-a-minor", "FFs3y4b18Dk", 4, 116, "49", [
+  section("V", "V1", [
+    phrase(6.32, "R1"),
+    phrase(14.07, "R2"),
+    phrase(22.81, "R3"), #R1 +12?
+    repeat(30.16, "R2", 40.26)
+  ])
+])
+
