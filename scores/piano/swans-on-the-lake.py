441d5d7a30059a67baff8be9aca3f26b3a034f9c
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-17 15:25:19 +0700

    transcribe swans on the lake structure and timestamps

---
+++ scores/piano/swans-on-the-lake.py
@@ -0,0 +1,17 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+piano(1000, "swans-on-the-lake", "ooYqqUTfW0Q", 4, 150, "40", [
+  section("V", "V1", [
+    phrase(3.17, "P1"),
+    phrase(7.98, "P2"),
+    repeat(12.47, "P1"),
+    repeat(17.14, "P2"),
+  ]),
+  section("V", "V2", [
+    phrase(21.63, "P5"),
+    phrase(26.07, "P6"),
+    repeat(30.60, "P1"),
+    phrase(35.10, "P8", [], 41.06)
+  ])
+])
+
