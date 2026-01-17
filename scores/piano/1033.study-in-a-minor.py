29b6a5aa3ed8e3ac8e09273e86c605ea05f1767c
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-17 14:58:35 +0700

    retranscribe study in a minor for a better video

--- scores/piano/1033.study-in-a-minor.py
+++ scores/piano/1033.study-in-a-minor.py
@@ -1,11 +1,11 @@
 # vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
 
-piano(1033, "study-in-a-minor", "FFs3y4b18Dk", 4, 116, "49", [
+piano(1033, "study-in-a-minor", "hy3epV4nshc", 4, 116, "49", [
   section("V", "V1", [
-    phrase(6.32, "R1"),
-    phrase(14.07, "R2"),
-    phrase(22.81, "R3"), #R1 +12?
-    repeat(30.16, "R2", 40.26)
+    phrase(0.34, "P1"),
+    phrase(9.15, "P2"),
+    phrase(19.55, "P3"),
+    repeat(28.49, "P2", 41.24)
   ])
 ])
 
