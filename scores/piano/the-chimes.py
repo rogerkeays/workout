731b34e69b8d48a1b34921994a69f7111c910076
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-17 15:51:04 +0700

    transcribe the chimes structure

---
+++ scores/piano/the-chimes.py
@@ -0,0 +1,11 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+piano(1001, "chimes", "rL2ah2oG1vM", 4, 114, "40", [
+  section("V", "V1", [
+    phrase(1.68, "P1"),
+    phrase(5.87, "P2"),
+    phrase(10.03, "P3"),
+    phrase(15.42, "P4", [], 19.44)
+  ])
+])
+
