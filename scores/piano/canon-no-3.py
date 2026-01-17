48a959034fe4779c8344675ebc97be62c64af8ff
Roger Keays <guybrush@mail8.simplelogin.com>
2026-01-17 16:04:39 +0700

    transcribe canon no 3 structure

---
+++ scores/piano/canon-no-3.py
@@ -0,0 +1,11 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+piano(1002, "canon-no-3", "vLngukh-Cv8", 4, 78, "40", [
+  section("V", "V1", [
+    phrase(11.18, "P1"),
+    phrase(16.80, "P2"),
+    phrase(20.12, "P3"),
+    phrase(25.01, "P4", [], 31.00)
+  ])
+])
+
