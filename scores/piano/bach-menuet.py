a79408b85a673ec0b04dac14c09a91f530972f3e
Roger Keays <guybrush@mail8.simplelogin.com>
2026-02-05 17:44:33 +0700

    transcribe bach menuet structure

---
+++ scores/piano/bach-menuet.py
@@ -0,0 +1,30 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+piano(1007, "bach-menuet", "akObhPYRId0", 3, 118, "40", [
+  section("V", "A1", [
+    phrase(1.87, "P01"),
+    phrase(4.83, "P02"),
+    phrase(7.88, "P03"),
+    phrase(10.97, "P04")
+  ]),
+  section("V", "A2", [
+    repeat(14.09, "P01"),
+    repeat(17.18, "P02"),
+    repeat(20.33, "P03"),
+    phrase(23.42, "P04")
+  ]),
+  section("V", "B", [
+    phrase(26.54, "P05"),
+    phrase(29.60, "P06"),
+    phrase(32.72, "P07"),
+    phrase(35.79, "P08")
+  ]),
+  section("V", "C", [
+    phrase(38.91, "P09"),
+    phrase(42.00, "P10"),
+    phrase(45.04, "P11"),
+    phrase(48.22, "P12", [], 51.89)
+  ]),
+])
+
+
