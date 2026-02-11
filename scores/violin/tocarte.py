ebdfd6883a02b099244221a4d9a5a55971ad6967
Roger Keays <guybrush@mail8.simplelogin.com>
2026-02-11 18:35:37 +0700

    transcribe tocarte toa structure

---
+++ scores/violin/tocarte.py
@@ -0,0 +1,120 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+# @club
+violin(4028, "tocarte", "1ZmvevvoprI", 4, 100, "49", [
+  section("I", "taxi", [
+    phrase(0.18, "0.wsws"),
+    phrase(2.60, "3.wsws"),
+    phrase(4.99, "8.wsws"),
+    phrase(7.39, "X.wsws")
+  ]),
+  section("A", "bar", [
+    phrase(9.80, "0.wwww"),
+    phrase(12.20, "3.wwww"),
+    phrase(14.59, "8.wwww"),
+    phrase(16.99, "X.wwww")
+  ]),
+  section("A", "bar", [
+    repeat(19.38, "0.wwww"),
+    repeat(21.79, "3.wwww"),
+    repeat(24.20, "8.wwww"),
+    repeat(26.58, "X.wwww")
+  ]),
+  section("B", "floor", [
+    phrase(28.99, "0.gggw"),
+    phrase(31.39, "3.gggw"),
+    phrase(33.79, "8.gggw"),
+    phrase(36.19, "X.gggw")
+  ]),
+  section("B", "floor", [
+    repeat(38.59, "0.gggw"),
+    repeat(40.99, "3.gggw"),
+    repeat(43.36, "8.gggw"),
+    repeat(45.79, "X.gggw")
+  ]),
+  section("C", "stage", [
+    phrase(48.19, "0.gggg"),
+    phrase(50.59, "3.gggg"),
+    phrase(52.99, "8.gggg"),
+    phrase(55.39, "X.gggg")
+  ]),
+  section("C", "stage", [
+    repeat(57.78, "0.gggg"),
+    repeat(60.19, "3.gggg"),
+    repeat(62.59, "8.gggg"),
+    repeat(64.99, "X.gggg")
+  ]),
+  section("B", "floor", [
+    repeat(67.38, "0.gggw"),
+    repeat(69.78, "3.gggw"),
+    repeat(72.18, "8.gggw"),
+    repeat(74.58, "X.gggw")
+  ]),
+  section("B", "floor", [
+    repeat(76.97, "0.gggw"),
+    repeat(79.39, "3.gggw"),
+    repeat(81.76, "8.gggw"),
+    repeat(84.19, "X.gggw")
+  ]),
+  section("D", "bathroom", [
+    phrase(86.60, "0.wwww.^"),
+    phrase(88.99, "3.wwww.^"),
+    phrase(91.39, "8.wwww.^"),
+    phrase(93.80, "X.wwww.^")
+  ]),
+  section("D", "bathroom", [
+    repeat(96.20, "0.wwww.^"),
+    repeat(98.60, "3.wwww.^"),
+    repeat(100.99, "8.wwww.^"),
+    repeat(103.39, "X.wwww.^")
+  ]),
+  section("C", "stage", [
+    repeat(105.79, "0.gggg"),
+    repeat(108.19, "3.gggg"),
+    repeat(110.59, "8.gggg"),
+    repeat(113.00, "X.gggg")
+  ]),
+  section("C", "stage", [
+    repeat(115.40, "0.gggg"),
+    repeat(117.79, "3.gggg"),
+    repeat(120.20, "8.gggg"),
+    repeat(122.59, "X.gggg")
+  ]),
+  section("B", "floor", [
+    repeat(124.99, "0.gggw"),
+    repeat(127.39, "3.gggw"),
+    repeat(129.78, "8.gggw"),
+    repeat(132.20, "X.gggw")
+  ]),
+  section("B", "floor", [
+    repeat(134.59, "0.gggw"),
+    repeat(136.99, "3.gggw"),
+    repeat(139.38, "8.gggw"),
+    repeat(141.79, "X.gggw")
+  ]),
+  section("A", "bar", [
+    repeat(144.20, "0.wwww"),
+    repeat(146.60, "3.wwww"),
+    repeat(148.99, "8.wwww"),
+    repeat(151.39, "X.wwww")
+  ]),
+  section("A", "bar", [
+    repeat(153.79, "0.wwww"),
+    repeat(156.20, "3.wwww"),
+    repeat(158.59, "8.wwww"),
+    repeat(160.98, "X.wwww")
+  ]),
+  section("O", "taxi", [
+    repeat(163.39, "0.wsws"),
+    repeat(165.79, "3.wsws"),
+    repeat(168.19, "8.wsws"),
+    repeat(170.60, "X.wsws")
+  ]),
+  section("O", "taxi", [
+    repeat(172.98, "0.wsws"),
+    repeat(175.39, "3.wsws"),
+    repeat(177.78, "8.wsws"),
+    repeat(180.20, "X.wsws", 182.99)
+  ])
+], video=False)
+
