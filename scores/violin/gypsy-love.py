0c9e7655edaad39d3dcfa0ee669997d14b678617
Roger Keays <guybrush@mail8.simplelogin.com>
2026-02-11 17:27:02 +0700

    transcribe gypsy love structure

---
+++ scores/violin/gypsy-love.py
@@ -0,0 +1,18 @@
+# vim: foldmethod=marker foldmarker=phrase,\ ) foldtext=getline(v\:foldstart)
+
+violin(1029, "gypsy-love", "Pl7YmHuVXfQ", 4, 135, "52", [
+  section("A", "shops", [
+    phrase(28.36, "eye"),         # he saw her in the street and caught her eye : s,wwss,sss : 0/77\6/7\30 /578
+    phrase(31.93, "gypsy"),       # he saw her in the street and said are you a gypsy? : s,wwss,www : \X/55\3/5\2X /335577
+    phrase(35.46, "date"),        # and they had an instant date, in a shopping mall : w,ssws,ssws : \5/78\0/2\0X /7\X/0\X8
+    phrase(39.47, "violin")       # then he took her to his camp to play the violin : wwww,wws : /00223355778\5/7
+  ]),
+  section("A'", "home", [
+    repeat(42.60, "eye"),
+    repeat(46.07, "gypsy"),
+    phrase(49.54, "bed"),         # and now they're in bed, and they've thrown off their clothes : s,swsw,sws : \7/0\L/02\0/23\20/5
+    phrase(52.83, "covers", [     # I can see there's only one way this will turn out : w,wwww,sxsx : \3/578 \75320L /00
+    ], 57.32)
+  ])
+])
+
