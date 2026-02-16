4e0e62e2e6f26968717030d04a22b9a03b57c4b1
Roger Keays <guybrush@mail8.simplelogin.com>
2026-02-16 15:54:19 +0700

    transcribe xmas melody structure

---
+++ scores/piano/xmas-melody.py
@@ -0,0 +1,72 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+# original: not available on youtube
+piano(1061, "xmas-melody", "original-xm", 4, 139, "42", [
+  section("I", "intro", [
+    phrase(3.09, "I1"),
+    phrase(5.64, "I2"),
+    phrase(8.23, "I3"),
+    phrase(10.85, "I4")
+  ]),
+  section("A", "4-4-R", [
+    phrase(13.62, "A1"),
+    phrase(17.08, "A2"),
+    phrase(20.54, "A3"),
+    phrase(24.03, "A4")
+  ]),
+  section("A", "4-4-L", [
+    phrase(27.57, "A1'"),
+    phrase(30.90, "A2'"),
+    phrase(34.17, "A3'"),
+    phrase(37.43, "A4'")
+  ]),
+  section("B", "3-2-D", [
+    phrase(40.72, "B1"),
+    phrase(43.97, "B2"),
+    phrase(47.20, "B3"),
+    phrase(50.42, "B4")
+  ]),
+  section("B", "3-2-D", [
+    repeat(53.59, "B1"),
+    repeat(56.66, "B2"),
+    repeat(59.76, "B3"),
+    repeat(62.66, "B4")
+  ]),
+  section("C", "melody", [
+    phrase(65.67, "C1"),
+    phrase(68.54, "C2"),
+    phrase(71.42, "C3"),
+    phrase(74.25, "C4")
+  ]),
+  section("D", "3-3-D", [
+    phrase(77.10, "D1-D"),
+    phrase(79.88, "D2-D"),
+    phrase(82.65, "D3-D"),
+    phrase(85.40, "D4-D")
+  ]),
+  section("D", "3-3-U", [
+    phrase(88.23, "D1-U"),
+    phrase(90.97, "D2-U"),
+    phrase(93.73, "D3-U"),
+    phrase(96.47, "D4-U")
+  ]),
+  section("E", "3-2-U", [
+    phrase(99.18, "E1"),
+    phrase(101.85, "E2"),
+    phrase(104.52, "E3"),
+    phrase(107.20, "E4")
+  ]),
+  section("B", "3-2-D", [
+    repeat(109.86, "B1"),
+    repeat(112.49, "B2"),
+    repeat(115.13, "B3"),
+    repeat(118.11, "B4")
+  ]),
+  section("B", "3-2-D", [
+    repeat(120.48, "B1"),
+    repeat(123.12, "B2"),
+    repeat(125.73, "B3"),
+    repeat(128.37, "B4", 136.93)
+  ])
+], video=False)
+
