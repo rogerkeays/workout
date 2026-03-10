5390fc99d388b880c596a230919acbbb8aac9c61
Roger Keays <guybrush@mail8.simplelogin.com>
2026-03-10 18:35:29 +0700

    transcribe symphony 40 structure

---
+++ scores/piano/symphony-40.py
@@ -0,0 +1,42 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+piano(2059, "symphony-40", "werQQ01SXI0", 4, 135, "47", [
+  section("I", "@bakery", [
+    phrase(10.72, "I1")
+  ]),
+  section("A", "@cabinet", [
+    phrase(14.03, "apple-pie"),  # -ap\ple -pie, /ap\ple -pie, /ap\ple -pie /theif : w swsw ss- : 87=87=87= 3 
+    phrase(17.60, "paranoid"),   # /looks \a\round, -pa\ra\noid, -he'll \be \spo-tted : (apple-pie) : 320 =T8 =75=
+    phrase(21.17, "custard"),    # \cus\tard -tarts, /cus\tard -tarts, /cus\tard -tarts /too : (apple-pie) : 75=75=75= 2
+    phrase(24.76, "kitten")      # /hears \a \sound, -from \a \pie, -just \a \ki-tten : (apple-pie) : 20L =87 =53=
+  ]),
+  section("B", "@till", [
+    phrase(28.20, "money"),      # /and \there's -mo/ney \in /the \till \too : (apple-pie) : 32 =5L207
+    phrase(31.75, "coins")       # -and \there's -mo/ney \in /the \till : w swsw s-- : 32 =5L20
+  ]),
+  section("C", "@floor", [
+    phrase(34.47, "falling"),    # /it's \fall\ing \to \the \floor : sww s : 320T87
+    phrase(36.30, "bag"),        # \care/fu/lly /put /it \in\to \the /bag : sss swss s : L0235320L
+    phrase(40.40, "pull"),       # /pull /tight, \pull /tight, \pull /tight : -s- s-s- s-s- s : 676767
+    phrase(45.61, "food")        # \let's \get /some \more \food : sss s-s- : 87875
+  ]),
+  section("A", "@cabinet", [
+    repeat(48.48, "apple-pie"),
+    repeat(52.51, "paranoid"),
+    repeat(56.03, "custard"),
+    repeat(59.60, "kitten")
+  ]),
+  section("B", "@till", [
+    repeat(63.03, "money"),
+    repeat(66.54, "coins"),
+  ]),
+  section("O", "@door", [
+    phrase(69.98, "door"),       # /at \the -door, /at \the -door, /at \the -door /a : (apple-pie) : 10= 10= 10= 8
+    phrase(73.62, "policeman"),  # /po\lice/man \with /no /head : k ssss s- : 5377L0
+    phrase(76.59, "head"),       # \no /head, \no /head : s- s-s- s- : L0L0
+    phrase(80.55, "dead", [      # -drop -dead : s- s-- : 0=
+    ], 83.97)
+  ])
+])
+
+
