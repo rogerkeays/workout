dd89706780df26e60e52918424f75af4b4fd0895
Roger Keays <>
2026-05-08 00:52:57 +0800

    transcribe king structure for voice

---
+++ scores/voice/king.py
@@ -0,0 +1,128 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+# @fridays
+voice(2002, "king", "KwXNHdfjh30", 4, 62.5, "49", [
+
+  # @club
+  section("V", "@stool", [
+    phrase(0.00, "V1"),   # disco: one night at the disco
+    phrase(4.10, "V2"),   # slow: i wanted to dance slow
+    phrase(7.89, "V3"),   # baby: i saw a sweet baby
+    phrase(11.90, "V4")   # lady: such a fine lady
+  ]),
+  section("V", "@dance-floor", [
+    repeat(15.30, "V1"),  # ask: and i walked up to ask her
+    repeat(19.36, "V2"),  # grabbed: but some dude just grabbed her
+    repeat(23.14, "V3"),  # back: i told him to back off
+    repeat(27.19, "V4")   # cut: cut me some slack off
+  ]),
+  section("W", "@pool-table", [
+    phrase(30.95, "W1"),  # coming: i see you come here
+    phrase(34.82, "W2"),  # beer: drinking that cheap beer
+    phrase(38.58, "W3"),  # own: act like you own it
+    phrase(42.33, "W4")   # condone: i can't condone it
+  ]),
+  section("X", "@alcove", [
+    phrase(45.82, "X1"),  # respect: you show me no respect
+    phrase(49.68, "X2"),  # off-it: you got to get off it
+    phrase(53.49, "X3"),  # with-it: you got to get with it
+    phrase(57.55, "X4")   # kick: time for a swift kick
+  ]),
+
+  # @bar
+  section("C", "@dishwashing-sink", [
+    phrase(61.41, "C1"),  # town: you see, i own this town
+    phrase(64.30, "C2"),  # best: you best not come around
+    phrase(67.17, "C3"),  # cool: if you wanna get by, then cool it down
+    phrase(70.93, "C4"),  # start: if you wanna start something
+    phrase(73.38, "C5")   # king: know one thing, i'm king
+  ]),
+  section("C", "@behind-bar", [
+    repeat(76.26, "C1"),  # mess: if you wanna mess around like that
+    repeat(79.50, "C2"),  # how: that's just how it is
+    repeat(82.36, "C3"),  # visit: if you wanna get by, then mind your biz
+    repeat(86.15, "C4"),  # start
+    repeat(88.50, "C5")   # king
+  ]),
+
+  # @beergarden: group-table balcony plants small-table
+  section("V", "@group-table", [
+    repeat(91.59, "V1"),  # attention: you wanted attention
+    repeat(95.50, "V2"),  # mention: and did i mention?
+    repeat(99.24, "V3"),  # nobody: nobody likes you
+    repeat(102.79, "V4")  # losers: except for the losers
+  ]),
+  section("V", "@veranda", [
+    repeat(106.52, "V1"), # army: you made a whole army
+    repeat(110.35, "V2"), # babies: an army of babies
+    repeat(114.28, "V3"), # stupid: some of them stupid
+    repeat(118.00, "V4")  # crazy: some of them crazy
+  ]),
+  section("W", "@plants", [
+    repeat(121.87, "W1"), # action: pick up your action
+    repeat(125.68, "W2"), # man: act like a grown man
+    repeat(129.41, "W3"), # winners: look at the winners
+    repeat(133.20, "W4")  # them: you can be like them
+  ]),
+  section("X", "@small-table", [
+    repeat(136.97, "X1"), # easy: life is so easy
+    repeat(140.76, "X2"), # dreamy: pleasant and dreamy
+    repeat(144.79, "X3"), # off-me: if you get off me
+    repeat(148.36, "X4")  # with-me: if you get with me
+  ]),
+
+  # bar
+  section("C", "@dishwashing-sink", [
+    repeat(152.14, "C1"), # town
+    repeat(155.25, "C2"), # best
+    repeat(157.87, "C3"), # cool
+    repeat(161.68, "C4"), # start
+    repeat(164.02, "C5")  # king
+  ]),
+  section("C", "@behind-bar", [
+    repeat(166.93, "C1"), # mess
+    repeat(170.20, "C2"), # how
+    repeat(173.04, "C3"), # visit
+    repeat(176.86, "C4"), # start
+    repeat(179.26, "C5")  # king
+  ]),
+
+  # middle: upstairs
+  section("M", "@upstairs", [
+    phrase(183.70, "M1"), # break-me: you can't break me
+    phrase(187.49, "M2"), # take-me: never take me
+    phrase(191.33, "M3")  # it-is: that's just how it is
+  ]),
+  section("M", "@upstairs", [
+    repeat(198.98, "M1"), # you can't break me
+    repeat(202.79, "M2"), # never shake me
+    repeat(206.58, "M3")  # clear out of my biz
+  ]),
+
+  # bar
+  section("C", "@dishwashing-sink", [
+    repeat(212.97, "C1"), # town
+    repeat(216.00, "C2"), # best
+    repeat(218.66, "C3"), # cool
+    repeat(222.44, "C4"), # start
+    repeat(224.80, "C5")  # king
+  ]),
+  section("C", "@behind-bar", [
+    repeat(227.62, "C1"), # mess
+    repeat(230.96, "C2"), # how
+    repeat(233.81, "C3"), # visit
+    repeat(237.59, "C4"), # start
+    repeat(239.95, "C5")  # king
+  ]),
+
+  # exit: downstairs bottom doors
+  section("O", "@exit", [
+    phrase(243.69, "O1"), # king: i'm king (x4)
+    phrase(251.07, "O2"), # king: i'm king (x4)
+    phrase(258.66, "S"),  # solo
+    phrase(273.91, "O3"), # king: i'm king (x4)
+    phrase(281.31, "O4"), # king: i'm king (x4)
+    phrase(288.68, "O5", [], 306.50)  # king: i'm k-----ing
+  ])
+], video=False)
+
