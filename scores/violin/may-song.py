9878eae76887a8f641aaf52c286566c57037c09a
Roger Keays <>
2026-05-13 22:14:28 +0800

    refactor may song score into sections

--- scores/violin/may-song.py
+++ scores/violin/may-song.py
@@ -1,11 +1,15 @@
 # vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
 
-violin(1035, "may-song", "H3I6lBOM-Xg", 4, 120, "49", [
-  section("A", "lake", [  # boat-island-boat
+violin(1035, "may-song", "H3I6lBOM-Xg", 4, 120, "49", [ # lake
+  section("A", "boat", [
     phrase(5.42, "spring"),   # spring is here, the birds a-are here : skss swsx : 0470 9097
     phrase(9.39, "singing"),  # listen to them singing : skss sxsx : 5740 20
-    phrase(13.52, "red"),      # red and yellow, purple and white : ssss swsx : /7755 4742
-    phrase(17.68, "sight"),    # oh my, what a beautiful sight : (red) : (red)
+  ]),
+  section("B", "island", [
+    phrase(13.52, "red"),     # red and yellow, purple and white : ssss swsx : /7755 4742
+    phrase(17.68, "sight"),   # oh my, what a beautiful sight : (red) : (red)
+  ]),
+  section("A", "boast", [
     repeat(21.79, "spring"),
     repeat(25.81, "singing", 30.44)
   ])
