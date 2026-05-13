b1a83f04ed0b0d0d063c01af8c0edc2982144434
Roger Keays <>
2026-05-13 21:30:41 +0800

    transcribe little children structure

---
+++ scores/violin/little-children.py
@@ -0,0 +1,29 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+violin(1034, "little-children", "ny3YVf8nbwk", 2, 78, 49, [ # nativity play
+  section("A", "hall", [
+    phrase(7.25, "children"),  # o come little children : ti matai mata : /774 774
+    phrase(10.36, "all"),       # o come one and all : ti matai ma : 75224
+    repeat(13.40, "children"),
+    repeat(16.48, "all")
+  ]),
+  section("B", "stage", [
+    phrase(19.48, "rejoicing"), # and see with rejoicing : (children) : 4222 55
+    phrase(22.53, "sight"),     # this glorious sight : (all) : 5444 9
+    phrase(25.51, "father"),    # our father in heaven : (children) : 9777 07
+    phrase(28.49, "night")      # has sent us this night : (all) : 45220
+  ]),
+  section("A", "hall", [
+    repeat(31.58, "children"),
+    repeat(34.61, "all"),
+    repeat(37.59, "children"),
+    repeat(40.58, "all")
+  ]),
+  section("B", "stage", [
+    repeat(43.43, "rejoicing"),
+    repeat(46.38, "sight"),
+    repeat(49.31, "father"),
+    repeat(52.30, "night", 56.03)
+  ]),
+])
+
