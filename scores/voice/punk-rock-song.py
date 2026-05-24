2b9d6e0007203f6961345b65356edc2c1fad5c4b
Roger Keays <>
2026-05-25 04:09:20 +0800

    transcribe punk rock song structure

---
+++ scores/voice/punk-rock-song.py
@@ -0,0 +1,74 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+voice(2003, "punk-rock-song", "sUli-Y0gZOs", 4, 176, "", [ # dystopia
+  section("I", "intro", [
+    phrase(0.48, "intro", skip="True"),
+  ]),
+  section("V", "desert", [
+    phrase(5.97, "dead"),         # have you been to the desert, have you walked with the dead?
+    phrase(8.71, "bread"),        # there's a hundred thousand children being killed for their bread
+    phrase(11.40, "lie"),         # and the figures don't lie, they speak of human disease
+    phrase(14.15, "think")        # but we do what we want and we think what we please
+  ]),
+  section("V", "hospital", [
+    phrase(16.89, "plague"),      # have you lived the experience, have you witnessed the plague?
+    phrase(19.75, "babies"),      # people making babies sometimes just to escape
+    phrase(22.08, "competition"), # in this land of competition, the compassion is gone
+    phrase(24.96, "needy"),       # yet, we ignore the needy and we keep pushing on
+    phrase(29.31, "keep")         # keep pushing on
+  ]),
+  section("C", "dump", [
+    phrase(30.66, "punk"),        # this is just a punk rock song
+    phrase(33.39, "people"),      # written for the people who can see something's wrong
+    phrase(35.88, "ants"),        # like ants in a colony, we do our share
+    phrase(38.44, "insects"),     # but there's so many other fuckin' insects out there
+    phrase(41.50, "song")         # and this is just a punk rock song
+  ]),
+  section("V", "quagmire", [
+    phrase(49.28, "shit"),        # have you visited the quagmire, have you swam in the shit?
+    phrase(52.13, "politik"),     # the party convention and the real politik
+    phrase(54.82, "faces"),       # the faces always different, the rhetoric the same
+    phrase(57.36, "swallow"),     # but we swallow it all and we see nothing change
+    phrase(61.72, "nothing")      # nothing has changed
+  ]),
+  section("C", "dump", [
+    repeat(63.03, "punk"),
+    repeat(65.76, "people"),
+    phrase(68.20, "workers"),     # like workers in a factory, we do our share
+    phrase(70.82, "robots"),      # but there's so many other fuckin' robots out there
+    repeat(73.90, "song")
+  ]),
+  section("S", "solo", [
+    phrase(76.65, "solo", skip="True"),
+  ]),
+  section("M", "tv", [
+    phrase(87.41, "million"),     # ten million dollars on a losing campaign
+    phrase(90.11, "starving"),    # twenty million starving and writhing in pain
+    phrase(92.89, "strong"),      # big, strong people, unwilling to give
+    phrase(95.48, "small")        # small in vision and perspective
+  ]),
+  section("W", "slum", [
+    phrase(101.70, "poverty"),    # one in five kids below the poverty line
+    phrase(104.42, "population"), # one population runnin' out of time,
+    phrase(108.45, "running"),    # runnin' out of time
+  ]),
+  section("C", "dump", [
+    repeat(109.77, "punk"),
+    repeat(112.50, "people"),
+    repeat(115.00, "ants"),
+    repeat(117.60, "insects"),
+    repeat(120.35, "song")
+  ]),
+  section("V", "ted", [
+    repeat(123.40, "lie"),
+    repeat(125.78, "think"),
+    repeat(128.79, "poverty"),
+    repeat(131.53, "population")
+  ]),
+  section("O", "stage", [
+    repeat(134.20, "punk"),
+    repeat(136.92, "punk"),
+    repeat(139.64, "punk", 144.45)
+  ])
+])
+
