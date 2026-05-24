5c1bdbad06d4b88745d7e7b95441e55ea8b89118
Roger Keays <>
2026-05-24 15:08:21 +0800

    transcribe long long ago structure with video

---
+++ scores/violin/long-long-ago.py
@@ -0,0 +1,17 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+violin(1036, "long-long-ago", "izADKOO2MVo", 4, 98, "49", [ # castle
+  section("A", "tower", [
+    phrase(6.50, "princess"),   # there was a princess with dark hair and eyes : swsw swsx : 002445 7974
+    phrase(11.42, "long-ago"),  # long long ago, long long ago : swsx swsx : 7542 5420
+    repeat(16.33, "princess"),
+    phrase(21.28, "book")       # long long ago, in a book : swsw sxxx : 7542 420
+  ]),
+  section("B", "bedroom", [
+    phrase(26.25, "emma"),      # long long ago, emma, long long ago : (princess) : /7542 \77 /5420
+    repeat(31.25, "emma"),
+    repeat(36.32, "princess"),
+    repeat(41.36, "book", 47.26)
+  ])
+])
+
