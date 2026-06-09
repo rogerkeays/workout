ef3fb91f3d5d8c2f1f6d5b984b21af7ce5fdd2a5
Roger Keays <>
2026-06-09 16:48:28 +0800

    transcribe what you're thinking vocals structure

---
+++ scores/voice/what-youre-thinking.py
@@ -0,0 +1,74 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+voice(2009, "what-youre-thinking", "nVl7g9b0oqk", 4, 158, "00", [ # lima-coast
+
+  # park
+  section("I", "park", [
+    phrase(0.20, "bard", skip=True),
+  ]),
+
+  # cliffs
+  section("V", "clifftop", [
+    phrase(13.24, "stone"),                # youre just a stones throw from me
+    phrase(16.21, "throwing"),             # well throwing stones could never be
+    phrase(19.95, "start")                 # the perfect way to start
+  ]),
+  section("V", "campfire", [
+    phrase(25.13, "sticks"),               # cause sticks and stones make broken bones
+    phrase(28.39, "empty-words"),          # empty words make broken homes
+    phrase(31.71, "heart")                 # but its love that broke ~our hearts
+  ]),
+  section("C", "beach", [
+    phrase(37.06, "crystal-ball"),         # and if i knew what you were thi~nking
+    phrase(43.29, "boat"),                 # i would stop this boat from si~nking
+    phrase(48.92, "sand"),                 # but darling you are like the sand
+    phrase(52.22, "slipping"),             # and youre slipping through my hands
+    phrase(55.34, "thinking")              # i just dont know what youre thinking
+  ]),
+
+  # path
+  section("S", "path", [
+    repeat(58.95, "bard", skip=True),
+  ]),
+
+  # house
+  section("V", "dresser", [
+    phrase(69.85, "drawer"),               # youre just a photo in a drawer
+    phrase(72.81, "photographs"),          # but photographs are not much more
+    phrase(76.49, "ghosts")                # than ghosts we cant forget
+  ]),
+  section("V", "entrance", [
+    phrase(81.75, "bull"),                 # youre just a red rag to a bull
+    phrase(84.71, "fool"),                 # youre just a false hope for a fool
+    phrase(88.02, "close")                 # but its as close as i can get
+  ]),
+  section("C", "veranda", [
+    repeat(93.65, "crystal-ball"),
+    repeat(99.85, "boat"),
+    phrase(105.61, "sun"),                  # but darling you are like the sun
+    phrase(108.69, "setting"),              # setting when my evening comes
+    repeat(112.00, "thinking")
+  ]),
+
+  # bungalow
+  section("M", "roof", [
+    phrase(117.63, "sky"),                  # and i would stop the sky from falling
+    phrase(122.32, "i-knew"),               # if i knew
+    phrase(125.40, "i--knew")               # if i~ knew
+  ]),
+  section("M", "bedroom", [
+    phrase(129.74, "morning"),              # and i would love you every morning
+    repeat(134.42, "i-knew"),
+    phrase(137.42, "i--knew-oh"),           # if i~ knew, oh oh ~oh
+  ]),
+
+  # stairs
+  section("C", "stairs", [
+    repeat(141.82, "crystal-ball"),
+    repeat(148.24, "boat"),
+    phrase(154.22, "stars"),                # but darling you are like the stars
+    phrase(157.49, "reach"),                # i know i cant reach that far
+    repeat(160.79, "thinking", 168.64)
+  ]),
+])
+
