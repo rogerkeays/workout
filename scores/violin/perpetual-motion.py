1800fee7f1ba72091c4b8808a016653501c0647a
Roger Keays <>
2026-06-11 13:59:31 +0800

    reinterpet perpetual motion structure

--- scores/violin/perpetual-motion.py
+++ scores/violin/perpetual-motion.py
@@ -5,28 +5,28 @@ violin(1038, "perpetual-motion", "wvKV3eUasW0", 4, 104, "49", [ # shopping-centr
     phrase(2.04, "intro", skip=True)
   ]),
   section("A1", "toy-shop", [
-    phrase(4.37, "escalator"),     # have you seen an alligator riding on an escalator?
-    phrase(8.98, "elevator"),      # i have seen an alligator riding on an elevator
-    phrase(13.61, "concertina"),   # have you seen a ballerina playing with a concertina?
-    phrase(18.27, "kitten")        # i have seen a ballerina playing with a kitten
+    phrase(4.37, "escalator_q"),    # have you seen an alligator riding on an escalator?
+    phrase(8.98, "escalator_a"),    # i have seen an alligator riding on an escalator
+    phrase(13.61, "concertina_q"),  # have you seen a ballerina playing with a concertina?
+    phrase(18.27, "concertina_a")   # i have seen a ballerina playing with a concertina
   ]),
   section("B1", "ikea", [
-    phrase(22.27, "armadillo"),    # have you seen an armadillo sleeping on a big fat pillow
-    phrase(27.47, "armadillo"),    # i have seen an armadillo sleeping on a big fat pillow, i have
-    repeat(32.18, "escalator"),
-    repeat(36.78, "elevator")
+    phrase(22.84, "armadillo"),     # have you seen an armadillo sleeping on a big fat pillow
+    repeat(27.49, "armadillo"),     # i have seen an armadillo sleeping on a big fat pillow
+    repeat(32.18, "escalator_q"),
+    repeat(36.78, "escalator_a")
   ]),
   section("A2", "toy-shop", [
-    phrase(41.42, "f_escalator"),
-    phrase(46.09, "f_elevator"),
-    phrase(50.65, "f_concertina"),
-    phrase(55.36, "f_kitten")
+    phrase(41.42, "f_escalator_q"),
+    phrase(46.09, "f_escalator_a"),
+    phrase(50.65, "f_concertina_q"),
+    phrase(55.36, "f_concertina_a")
   ]),
   section("B2", "ikea", [
-    phrase(59.42, "f_armadillo"),
-    phrase(64.63, "f_car"),
-    repeat(69.21, "f_escalator"),
-    repeat(73.78, "f_elevator", 79.54)
+    phrase(60.03, "f_armadillo"),
+    repeat(64.65, "f_armadillo"),
+    repeat(69.21, "f_escalator_q"),
+    repeat(73.78, "f_escalator_a", 79.54)
   ])
 ])
 
