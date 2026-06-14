8804886a20337d3a4e45ce72ab6a04663ccfa46a
Roger Keays <>
2026-06-14 14:04:32 +0800

    transcribe in the woods structure

---
+++ scores/violin/in-the-woods.py
@@ -0,0 +1,20 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+violin(1039, "in-the-woods", "PCdobC1MIG4", 4, 140, "42", [ # forest
+  section("I", "intro", [
+    phrase(1.18, "intro", skip=True)
+  ]),
+  section("A", "glade", [
+    phrase(4.68, "woods"),   # in the woods, far away, on a hot summers day : (w sws * 2) : 024475 597754
+    phrase(8.02, "cabin"),   # the three bears cabin stood, at the edge of the wood : (woods) : 475542 254420
+    repeat(11.48, "woods"),
+    phrase(14.80, "walk")     # the three bears went away for a walk : (woods drop 3) : 475542 420
+  ]),
+  section("B", "path", [
+    phrase(18.28, "papa"),    # first the big papa bear, then the kind mama bear : (woods) : 042\777 /254000
+    phrase(21.70, "chair"),   # then behind with his chair came the small baby bear : (woods) : 475 597 707542
+    repeat(27.27, "woods"),
+    repeat(31.12, "walk", 35.44)
+  ])
+])
+
