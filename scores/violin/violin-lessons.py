bed586e27bcd878fc9d7a47b73d6157d550a21e1
Roger Keays <>
2026-06-16 14:52:56 +0800

    transcribe violin lessons structure

---
+++ scores/violin/violin-lessons.py
@@ -0,0 +1,21 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+violin(1040, "violin-lessons", "49XbZAg08IQ", 4, 134, "49", [
+  section("I", "intro", [
+    phrase(2.62, "intro", skip=True)
+  ]),
+  section("A", "lesson", [
+    phrase(6.70, "suzuki"),    # suzuki is the right method to take : w swsw ssx : 457 542 4202
+    phrase(10.29, "violin"),   # on our small violin music we make : (suzuki) : 457 79Y 0Y97
+    repeat(13.79, "suzuki"),
+    phrase(17.37, "break")     # after we finish this, take a small break : (suzuki) : 457 9Y0 \5420
+  ]),
+  section("B", "exit", [
+    phrase(20.97, "listen"),   # make sure that you listen each day : s sssw ssx : /4407 9Y07
+    phrase(24.61, "play"),     # this will really help you to play : s ssss ssx : /20Y97542
+    repeat(28.92, "suzuki"),
+    repeat(32.57, "break", 39.06)
+  ])
+])
+
+
