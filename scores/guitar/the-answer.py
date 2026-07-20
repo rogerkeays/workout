287f42865d949d38ad8b54c10cf68971b16d414b
Roger Keays <>
2026-07-20 22:12:03 +0700

    normal the answer guitar score

--- scores/guitar/the-answer.py
+++ scores/guitar/the-answer.py
@@ -1,30 +1,30 @@
 # vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
 
 guitar(2001, "the-answer", "9A2KH0sRQUU", 4, 139, "44", [
-  section("I", "@library", [
+  section("I", "library", [
     phrase(1.12, "I1"),
     phrase(8.03, "I2")
   ]),
-  section("V", "@church", [
-    phrase(9.77, "V1"),    # pews : long ago in a dusty village full of hunger pa-in a-nd strife
-    phrase(16.73, "V2"),   # priest : a man came forth with a vision of truth and a way to a be~tte-r life
-    phrase(23.65, "V3"),   # pulpit : he was convinced he had the answer and he compelled people to follow a~long
-    phrase(30.60, "V4"),   # exit : but the hunger never vanished and the man was vanished and the vi~llage dried up a~nd died
+  section("V", "village", [
+    phrase(9.77, "V1"),            # long ago in a dusty village full of hunger pa-in a-nd strife
+    phrase(16.73, "V2"),           # a man came forth with a vision of truth and a way to a be~tte-r life
+    phrase(23.65, "V3"),           # he was convinced he had the answer and he compelled people to follow a~long
+    phrase(30.60, "V4"),           # but the hunger never vanished and the man was vanished and the vi~llage dried up a~nd died
     phrase(37.53, "V5")
   ]),
-  section("V", "@lab", [
-    repeat(39.25, "V1"),   # telescope : at a time when wise me peered through glass tubes towards the sky
-    repeat(46.21, "V2"),   # heavens : the heavens changed in predictable ways that one man was able t-o find
-    repeat(53.17, "V3"),   # writing : and he had thought he'd found the answer and was quick to write his revela-tions
-    repeat(60.16, "V4"),   # but as they were scrutinised in his colleagues e-yes, he soon became a m-o-cke-ry
+  section("V", "lab", [
+    repeat(39.25, "V1"),           # at a time when wise me peered through glass tubes towards the sky
+    repeat(46.21, "V2"),           # the heavens changed in predictable ways that one man was able t-o find
+    repeat(53.17, "V3"),           # and he had thought he'd found the answer and was quick to write his revela-tions
+    repeat(60.16, "V4"),           # but as they were scrutinised in his colleagues e-yes, he soon became a m-o-cke-ry
     repeat(67.14, "V5")
   ]),
-  section("C", "@classroom", [
-    phrase(68.84, "C1"),   # teacher : don't tell me about the answer, 'cause then another one will come alo-ng soon
-    phrase(75.87, "C2"),   # student : don't tell me about the answer, i've got ideas too
-    phrase(82.92, "C3")    # sheep : but if you've got enough na-ïvity, and you've got conviction, then the answer is perfect fo-r you
+  section("C", "classroom", [
+    phrase(68.84, "C1"),           # don't tell me about the answer, 'cause then another one will come alo-ng soon
+    phrase(75.87, "C2"),           # don't tell me about the answer, i've got ideas too
+    phrase(82.92, "C3")            # but if you've got enough na-ïvity, and you've got conviction, then the answer is perfect fo-r you
   ]),
-  section("S", "@solo", [
+  section("S", "solo", [
     phrase(87.74, "S1"),
     phrase(95.21, "S2"),
     phrase(102.40, "S3"),
@@ -32,18 +32,18 @@ guitar(2001, "the-answer", "9A2KH0sRQUU", 4, 139, "44", [
     repeat(113.08, "I1"),
     repeat(120.09, "I2")
   ]),
-  section("V", "@city", [
-    repeat(121.86, "V1"),  # beggar : an urban sprawl sits choking on it's discharge, overwhelmed by i~ndustry
-    repeat(128.91, "V2"),  # musk : searching for another .. saviour from another place inclined toward cha~ri~ty
-    repeat(135.94, "V3"),  # crystals : everyone begging for an answer without regard to vali~di~ty
-    repeat(142.98, "V4"),  # falkor : but the search it never ends it goes o-n a-nd o-on a-nd on for eter~ni~ty
+  section("V", "city", [
+    repeat(121.86, "V1"),          # an urban sprawl sits choking on it's discharge, overwhelmed by i~ndustry
+    repeat(128.91, "V2"),          # searching for another .. saviour from another place inclined toward cha~ri~ty
+    repeat(135.94, "V3"),          # everyone begging for an answer without regard to vali~di~ty
+    repeat(142.98, "V4"),          # but the search it never ends it goes o-n a-nd o-on a-nd on for eter~ni~ty
     repeat(149.95, "V5")
   ]),
-  section("M", "@valley", [
-    phrase(151.71, "M1"),  # singers : ah, ah, ah~~ ah
-    repeat(158.70, "M1")   # singers : ah, ah, ah~~ ah
+  section("M", "hills", [
+    phrase(151.71, "M1"),          # ah, ah, ah~~ ah
+    repeat(158.70, "M1")           # ah, ah, ah~~ ah
   ]),
-  section("C", "@classroom", [
+  section("C", "classroom", [
     repeat(165.78, "C1"),
     repeat(172.80, "C2"),
     repeat(179.83, "C3"),
