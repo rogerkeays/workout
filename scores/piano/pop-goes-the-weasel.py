567a67f1669b4a4cc1f616d6d5549db92c972a5e
Roger Keays <>
2026-07-20 13:01:44 +0700

    patch tempo

--- scores/piano/pop-goes-the-weasel.py
+++ scores/piano/pop-goes-the-weasel.py
@@ -1,12 +1,12 @@
 # vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
 
 # bastien piano basics level 1
-piano(1032, "pop-goes-the-weasel", "WBFs6xr2Kjg", 3, 150, "40", [
+piano(1032, "pop-goes-the-weasel", "WBFs6xr2Kjg", 3, 100, "40", [
   section("A", "V1", [
     phrase(0.70, "A1"),
     phrase(4.98, "A2"),
     repeat(9.82, "A1"),
     phrase(15.09, "A3", [], 20.41)
   ])
-], speeds=[1.0, 1.5])
+])
 
