ee018e409051a756c7127bf6fe8b76a90f2c0e7b
Roger Keays <>
2026-07-20 15:56:41 +0700

    transcribe fall at your feet vocals structure

---
+++ scores/voice/fall-at-your-feet.py
@@ -0,0 +1,60 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+voice(2010, "fall-at-your-feet", "2ROiU9kOHeE", 4, 98, 43, [
+  section("I", "her-office", [
+    phrase(0.86, "intro", skip=True)
+  ]),
+  section("V", "his-room", [
+    phrase(13.59, "close"),         #  -im.rea-lly.close-to'ni/.,ght.../.. : 40L974
+    phrase(18.01, "inside"),        # ,n-i.fee,l'like.,im/.mo-ving.in-side.-her./.. : 470L97 42L24
+    phrase(23.14, "dark"),          # larng'in.,the-da/.'a.ark../ : (4)0L99 74
+    phrase(27.39, "beginning"),     # ,n-i.think-that'im.-be=gi/.=ning'to.-know.-her. : =inside (+4) 
+    phrase(32.10, "go"),            # .let,it'go/.'o.o../. : 4209
+    phrase(35.33, "call")           # -ill.be-the.re-when'you.-call/..../. : 9097420
+  ]),
+  section("C", "temple", [
+    phrase(40.05, "fall"),          # -and.-when.e=ver'i.-fa/...,a-all./ :: 000L24 20
+    phrase(44.44, "feet"),          # .at..your.-fee/...,e-eet./.. :: 720 L9
+    phrase(50.02, "tears"),         # -you.let-you.r-tea/..,e-ars../ :: =fall (-0)
+    phrase(53.83, "rain"),          # .rain..down.-on/.-me.../.. :: =feet (+0)
+    phrase(59.34, "touch"),         # whenever i tou~~ch : /mat -and :: =fall (-0)
+    phrase(62.85, "pain")           # (-your/).-slow..turn.ing'pain/..../.. :: =feet (+7) (+0)
+  ]),
+  section("V", "her-doorstop", [
+    phrase(68.65, "hiding"),        # -youre.hi,,ding'from.,me'no/.'o.ow../.. :: 900L99 74
+    phrase(73.29, "talking"),       # -theres.some=thing'in.-the/.way-that.youre-talk.,ing./.. :: =inside (-7)
+    phrase(78.09, "words"),         # .words-dont.-sou/.nd-right.../.. :: 0LL99
+    phrase(82.55, "hear"),          # ,n-i.he-ar'them.,all/.mo-ving.in-side/.you. :: =inside
+    phrase(86.44, "no"),            # /.no-o.../. :: 09
+    phrase(89.07, "waiting")        # i'll be waiting when you call :: =call
+  ]),
+  section("C", "temple", [
+    repeat(93.12, "fall"),          # and<1
+    repeat(98.13, "feet"),
+    repeat(103.71, "tears"),
+    repeat(107.53, "rain"),
+    repeat(113.08, "touch"),
+    repeat(116.56, "pain")
+  ]),
+  section("B", "pool", [
+    phrase(122.38, "blame"),         # -the.fin-ger'of.-bla/.me-has.-turned.-up.-on/.-it.-self. :: 2202?40000L00
+    phrase(127.66, "offer"),         # -n'im.-mo/.re-than.-willn.-to.-o/.ffer-my.-self. :: 00400000L000
+    phrase(132.28, "presence"),      # -do.you-wa/.nt-my.-pre.sence-or.-need/.-my.-help.. :: 44499975
+    phrase(137.45, "lead")           # -who/.-knows.-where.that-might.-le/.'e.ad.. :: 
+  ]),
+  section("B", "diving", [
+    phrase(142.14, "faaall", stop=147.72)  # -i/.-fall.../.... :: 4^4
+  ]),
+  section("C", "temple", [
+    repeat(159.63, "fall"),          # -and
+    repeat(163.38, "feet"),
+    repeat(168.59, "tears"),         # .you-let.yo.ur-te/_
+    repeat(172.65, "rain"),          # _/.-me..,e-e./..
+    repeat(178.00, "fall"),          # _/..../.. :: 44207
+    repeat(183.06, "fall")           # ever i fall/..../.... :: 4209
+  ]),
+  section("O", "ground", [
+    phrase(188.56, "outro", stop=193.13, skip=True),
+  ]),
+])
+
