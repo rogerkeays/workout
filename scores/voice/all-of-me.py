4c1491d0c58527b78a99c0a6960a3685fc753062
Roger Keays <>
2026-08-05 13:07:57 +0700

    refactor all of me score for voice

---
+++ scores/voice/all-of-me.py
@@ -0,0 +1,460 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+voice(3012, "all-of-me", "450p7goxZqg", 4, 126, "48", [ # wedding-reception
+  section("V", "art-desk", [
+    phrase(17.51, "mouth", notes("""
+      3 2 L4== what
+      e 4 ==== would
+      4 = ==== i
+      1 = ==== do
+      a = ==== with-
+      2 2 ==== o-
+      u 4 ==== ut
+      3 = ===~ your
+      4 0 ===~ smart
+      1 = ===~ mouth?
+      2 Z ==== ."""
+    )),
+    phrase(21.31, "drawin", notes("""
+      3 2 L4== 2 draw-
+      e 4 ==== v in
+      4 = ==== 6 me
+      1 = ==== 2 in
+      a 2 ==== v a
+      2 4 ==== 6 you
+      3 = ==== 2 kick-
+      e 2 ==== 4 in
+      4 0 ==== 2 me
+      o = ==== 4 a-
+      1 9 ==== 2 out
+      2 Z ==== 6 ."""
+    )),
+    phrase(25.12, "spinnin", notes("""
+      u 9 L4== you've
+      3 4 ==== got
+      t = ==== my
+      1 5 ==== head
+      3 4 ==== spin-
+      e 0 ==== nin
+      4 Z ==== ,
+      1 5 ==== no
+      3 4 ==== kid-
+      e 0 ==== din
+      4 Z ==== ."""
+    )),
+    phrase(29.85, "pin", notes("""
+      o 0 L4== i
+      1 2 ==== can't
+      u Z ==== ,
+      3 4 ==== pin
+      e 2 ==== yo-
+      o 9 ==== u
+      1 = ==== down
+      u Z ==== ."""
+    ))
+  ]),
+  section("V", "disneyland", [
+    phrase(32.95, "mind", notes("""
+      3 2 L4== what's
+      e 4 ==== go-
+      4 = ==== in
+      1 = ==== on
+      a 2 ==== in
+      2 4 ==== that
+      3 = ==== beau-
+      e 2 ==== ti-
+      4 0 ==== ful
+      w = ==== mind
+      2 Z ==== ?"""
+    )),
+    phrase(36.67, "ride", notes("""
+      3 4 L4== i'm
+      e = ==== on
+      4 = ==== your
+      1 7 ==== ma-
+      a 5 ==== gi-
+      2 4 ==== cal
+      3 = ==== mys-
+      e 2 ==== te-
+      4 0 ==== ry-
+      o = ==== r-
+      1 9 ==== ide
+      u Z ==== ."""
+    )),
+    phrase(40.57, "dizzy", notes("""
+      3 4 L4== and
+      e = ==== i'm
+      1 5 ==== so
+      3 4 ==== di-
+      e 0 ==== zzy
+      4 Z ==== ,
+      o = ==== don't
+      1 4 ==== kn-
+      b 5 ==== ow
+      2 = ==== what
+      3 4 ==== hit
+      e 0 ==== me
+      4 Z ==== ."""
+    )),
+    phrase(45.03, "alright", notes("""
+      o 0 L4== but
+      1 2 ==== i'll
+      3 4 ==== be
+      e 2 ==== al-
+      1 5 ==== right
+      e Z ==== ."""
+    ))
+  ]),
+  section("B", "show", [
+    phrase(48.92, "underwater", notes("""
+      o 0 L4== my
+      1 7 ==== h-
+      b 9 ==== ead's
+      u 7 ==== u-
+      3 5 ==== n-
+      e = ==== de-
+      4 4 ==== r
+      o = ==== wa-
+      u 2 ==== te-
+      3 0 ==== r
+      e = ==== but
+      4 Z ==== ."""
+    )),
+    phrase(52.71, "breathin", notes("""
+      o Y L4== i'm
+      u 9 ==== br-
+      3 7 ==== ea-
+      e = ==== th-
+      4 9 ==== in
+      o = ==== fine
+      u Z ==== ."""
+    )),
+    phrase(56.57, "crazy", notes("""
+      o 7 L4== y-
+      w 9 ==== ou're
+      u = ==== cr-
+      3 7 ==== a-
+      e = ==== zy
+      4 5 ==== and
+      o 4 ==== i'm
+      u = ==== o-
+      3 2 ==== ut
+      e = ==== of
+      4 0 ==== my
+      o 2 ==== mind
+      3 Z ==== ."""
+    ))
+  ]),
+  section("CA", "mirrors", [
+    phrase(62.21, "all-me", notes("""
+      o 4 L4== cause
+      1 = ==== a-
+      b 7 ==== a-
+      3 = ==== ll
+      4 4 ==== of
+      o 7 ==== m-
+      1 9 ==== e
+      2 Z ==== ."""
+    )),
+    phrase(65.10, "all-you", notes("""
+      3 4 L4== loves
+      1 2 ==== all
+      4 0 ==== of
+      o 2 ==== y-
+      1 4 ==== ou
+      2 Z ==== ."""
+    )),
+    phrase(68.86, "curves", notes("""
+      3 4 L4== love
+      e = ==== you-
+      o 2 ==== r
+      1 = ==== curves
+      2 = ==== and
+      3 = ==== all
+      4 0 ==== your
+      o 2 ==== ed-
+      1 0 ==== ges
+      2 Z ==== ."""
+    )),
+    lyrics(72.86, "imperfections", "all you-r per-fect im-per-fec-tions", "curves")
+  ]),
+  section("CB", "sepulchre", [
+    phrase(76.63, "give-your", notes("""
+      3 4 L4== give
+      e = ==== your
+      1 = ==== a-
+      b 7 ==== ll
+      4 4 ==== to
+      o 7 ==== m-
+      1 9 ==== e
+      2 Z ==== ."""
+    )),
+    phrase(80.35, "give-my", notes("""
+      u 2 L4== i'll
+      3 9 ==== give
+      e 4 ==== m-
+      o 2 ==== y
+      1 = ==== all
+      4 0 ==== to
+      o 2 ==== yo-
+      1 4 ==== u
+      2 Z ==== ."""
+    )),
+    lyrics(84.10, "end", "you're m-y end and my be-gin-ing", "curves"),
+    lyrics(88.17, "lose", "e-ve-n when i lose i'm win-ning", "curves")
+  ]),
+  section("CO", "altar", [
+    phrase(91.70, "i-give", notes("""
+      u 4 L4== cause
+      3 = ==== i
+      e 5 ==== give
+      4 7 ==== you
+      o 0 ==== a-
+      u Y ==== u-
+      p 0 ==== i-
+      3 Y ==== a-
+      o 9 ==== a-
+      u 7 ==== ll
+      3 Z ==== ,
+      4 4 ==== of
+      o = ==== me
+      u Z ==== ."""
+    )),
+    lyrics(99.29, "you-give", "and you give me a-u-i-a-a-ll of you", "i-give"),
+    phrase(105.83, "oo", notes("""
+      4 4 L4== oh-
+      o 2 ==== o
+      3 Z ==== ."""
+    ))
+  ]),
+  section("V", "bedroom", [
+    phrase(109.05, "times", notes("""
+      3 2 L4== how
+      e 4 ==== ma-
+      4 2 ==== ni-
+      m 4 ==== y
+      1 2 ==== t-
+      a 4 ==== imes
+      2 = ==== do
+      u 2 ==== i
+      3 4 ==== have
+      e 2 ==== to
+      4 0 ==== tell
+      1 = ==== you?
+      2 Z ==== ."""
+    )),
+    phrase(112.98, "crying", notes("""
+      3 4 L4== e-
+      e = ==== ven
+      4 = ==== when
+      o = ==== you're
+      1 5 ==== c-
+      b 7 ==== ry-
+      2 5 ==== ing
+      u 4 ==== you're
+      3 = ==== beau-
+      e 2 ==== ti-
+      4 0 ==== ful
+      o = ==== t-
+      1 9 ==== oo
+      2 Z ==== ."""
+    )),
+    phrase(116.55, "beating", notes("""
+      u 0 L4== the
+      3 2 ==== w-
+      c 4 ==== orld
+      t = ==== is
+      1 5 ==== beat-
+      u 4 ==== i-
+      p 5 ==== i-
+      3 4 ==== ing
+      e 0 ==== you
+      o 4 ==== d-
+      1 5 ==== own
+      u 4 ==== i-
+      e 0 ==== m
+      4 = ==== a-
+      o 2 ==== round
+      2 Z ==== ."""
+    )),
+    phrase(122.17, "mood", notes("""
+      u 2 L4== through
+      3 4 ==== e-
+      e 2 ==== ve-
+      4 = ==== ry
+      o 9 ==== mood
+      2 Z ==== ."""
+    ))
+  ]),
+  section("V", "garage", [
+    phrase(124.19, "downfall", notes("""
+      p 7 L4== you're
+      e 4 ==== my
+      o = ==== down-
+      a 0 ==== fall,
+      3 7 ==== you're
+      e 4 ==== my
+      o = ==== muse
+      a Z ==== ."""
+    )),
+    phrase(127.97, "distraction", notes("""
+      u 0 L4== my
+      3 5 ==== w-
+      e 7 ==== orst
+      4 4 ==== dis-
+      o = ==== tra-
+      1 2 ==== ac-
+      a = ==== tion,
+      u = ==== my
+      3 4 ==== rhy-
+      e 2 ==== thm
+      4 4 ==== n
+      o 9 ==== blues
+      2 Z ==== ."""
+    )),
+    phrase(131.69, "singing", notes("""
+      u 4 L4== i
+      3 = ==== can't
+      t = ==== stop
+      1 5 ==== si-
+      u 4 ==== ngi-
+      3 0 ==== ing
+      e = ==== it's
+      4 Z ==== ,
+      o 4 ==== ri-
+      1 5 ==== i-
+      u 4 ==== ngi-
+      3 0 ==== ing
+      e = ==== in
+      4 Z ==== ."""
+    )),
+    phrase(136.49, "head", notes("""
+      o 2 L4== my
+      2 Z ==== ,
+      u 4 ==== h-
+      e 2 ==== ead
+      4 = ==== for
+      o 4 ==== y-
+      1 5 ==== ou
+      4 Z ==== ."""
+    ))
+  ]),
+  section("B", "show", [
+    repeat(140.37, "underwater"),
+    repeat(144.29, "breathin"),
+    repeat(147.91, "crazy")
+  ]),
+  section("CA", "mirrors", [
+    repeat(153.74, "all-me"),
+    repeat(156.63, "all-you"),
+    repeat(160.38, "curves"),
+    repeat(164.37, "imperfections")
+  ]),
+  section("CB", "sepulchre", [
+    repeat(169.05, "give-your"),
+    phrase(171.75, "give-my^", notes("""
+      u 4 L4== i'll
+      3 0 ==== give
+      e Y ==== m-
+      o 9 ==== y
+      1 = ==== all
+      4 7 ==== to
+      o 4 ==== you
+      2 Z ==== ."""
+    )),
+    repeat(175.63, "end"),
+    repeat(179.64, "lose"),
+  ]),
+  section("CO", "altar", [
+    repeat(183.20, "i-give"),
+    repeat(190.79, "you-give")
+  ]),
+  section("M", "casino", [
+    phrase(198.06, "give-me", notes("""
+      a 5 L4== give
+      2 7 ==== me
+      u Y ==== a-
+      3 0 ==== ll
+      e Y ==== of
+      4 9 ==== you
+      u 7 ==== o-
+      p 5 ==== u-
+      3 4 ==== o-
+      c 2 ==== u-
+      e 0 ==== oh
+      1 Z ==== ."""
+    )),
+    phrase(201.59, "cards", notes("""
+      1 9 L4== cards
+      u 7 ==== o-
+      3 5 ==== n
+      e = ==== the
+      o Y ==== t-
+      1 0 ==== a-
+      u Y ==== b-
+      3 9 ==== le
+      e = ==== w-
+      4 7 ==== e're
+      o = ==== both
+      u 5 ==== sh-
+      3 4 ==== o-
+      e = ==== w-
+      4 2 ==== ing
+      o = ==== hearts
+      e Z ==== ."""
+    )),
+    phrase(209.01, "risk", notes("""
+      1 9 L4== ris-
+      u 7 ==== king
+      3 5 ==== it
+      o 5 ==== a-
+      1 0 ==== ll
+      u Y ==== th-
+      3 9 ==== ough
+      e = ==== it's
+      o 7 ==== hard
+      3 Z ==== ."""
+    ))
+  ]),
+  section("CA", "mirrors", [
+    repeat(214.54, "all-me"),
+    repeat(217.52, "all-you"),
+    repeat(221.34, "curves"),
+    repeat(225.31, "imperfections")
+  ]),
+  section("CB", "sepulchre", [
+    repeat(229.15, "give-your"),
+    repeat(232.65, "give-my^"),
+    repeat(236.60, "end"),
+    repeat(240.55, "lose")
+  ]),
+  section("CO", "altar", [
+    repeat(244.20, "i-give"),
+    repeat(251.77, "you-give")
+  ]),
+  section("CO", "altar", [
+    phrase(259.26, "i-give-auaua", notes("""
+      3 4 L4== i
+      e 5 ==== give
+      4 7 ==== you
+      o 0 ==== a-
+      2 2 ==== u-
+      u Y ==== a-
+      4 0 ==== u-
+      o 9 ==== a-
+      u 7 ==== ll
+      3 Z ==== ,
+      4 4 ==== of
+      o = ==== me
+      3 Z ==== ."""
+    )),
+    repeat(267.04, "you-give"),
+    phrase(273.42, "ouo", notes("""
+      4 2 L4== o-
+      o 4 ==== u-
+      1 2 ==== oh
+      4 Z ==== ."""
+    ), 282.03)
+  ])
+])
+
