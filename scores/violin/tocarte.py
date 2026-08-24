eb3a7f0788c7eab93285e81b285a58c2b19ea6ae
Roger Keays <>
2026-08-24 19:08:58 +0800

    sync tocarte toa to new video

--- scores/violin/tocarte.py
+++ scores/violin/tocarte.py
@@ -1,119 +1,119 @@
 # vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
 
-violin(4028, "tocarte", "1ZmvevvoprI", 4, 100, "49", [ # club
+violin(4028, "tocarte", "nxnNMY6Dlvg", 4, 100, "49", [ # club
   section("I", "taxi", [
-    phrase(0.18, "0.wsws"),
-    phrase(2.60, "3.wsws"),
-    phrase(4.99, "8.wsws"),
-    phrase(7.39, "X.wsws")
+    phrase(0.14, "0.wsws"),
+    phrase(2.56, "3.wsws"),
+    phrase(4.95, "8.wsws"),
+    phrase(7.35, "X.wsws")
   ]),
   section("A", "bar", [
-    phrase(9.80, "0.wwww"),
-    phrase(12.20, "3.wwww"),
-    phrase(14.59, "8.wwww"),
-    phrase(16.99, "X.wwww")
+    phrase(9.76, "0.wwww"),
+    phrase(12.16, "3.wwww"),
+    phrase(14.55, "8.wwww"),
+    phrase(16.95, "X.wwww")
   ]),
   section("A", "bar", [
-    repeat(19.38, "0.wwww"),
-    repeat(21.79, "3.wwww"),
-    repeat(24.20, "8.wwww"),
-    repeat(26.58, "X.wwww")
+    repeat(19.34, "0.wwww"),
+    repeat(21.75, "3.wwww"),
+    repeat(24.16, "8.wwww"),
+    repeat(26.54, "X.wwww")
   ]),
   section("B", "floor", [
-    phrase(28.99, "0.gggw"),
-    phrase(31.39, "3.gggw"),
-    phrase(33.79, "8.gggw"),
-    phrase(36.19, "X.gggw")
+    phrase(28.95, "0.gggw"),
+    phrase(31.35, "3.gggw"),
+    phrase(33.75, "8.gggw"),
+    phrase(36.15, "X.gggw")
   ]),
   section("B", "floor", [
-    repeat(38.59, "0.gggw"),
-    repeat(40.99, "3.gggw"),
-    repeat(43.36, "8.gggw"),
-    repeat(45.79, "X.gggw")
+    repeat(38.55, "0.gggw"),
+    repeat(40.95, "3.gggw"),
+    repeat(43.32, "8.gggw"),
+    repeat(45.75, "X.gggw")
   ]),
   section("C", "stage", [
-    phrase(48.19, "0.gggg"),
-    phrase(50.59, "3.gggg"),
-    phrase(52.99, "8.gggg"),
-    phrase(55.39, "X.gggg")
+    phrase(48.15, "0.gggg"),
+    phrase(50.55, "3.gggg"),
+    phrase(52.95, "8.gggg"),
+    phrase(55.35, "X.gggg")
   ]),
   section("C", "stage", [
-    repeat(57.78, "0.gggg"),
-    repeat(60.19, "3.gggg"),
-    repeat(62.59, "8.gggg"),
-    repeat(64.99, "X.gggg")
+    repeat(57.74, "0.gggg"),
+    repeat(60.15, "3.gggg"),
+    repeat(62.55, "8.gggg"),
+    repeat(64.95, "X.gggg")
   ]),
   section("B", "floor", [
-    repeat(67.38, "0.gggw"),
-    repeat(69.78, "3.gggw"),
-    repeat(72.18, "8.gggw"),
-    repeat(74.58, "X.gggw")
+    repeat(67.34, "0.gggw"),
+    repeat(69.74, "3.gggw"),
+    repeat(72.14, "8.gggw"),
+    repeat(74.54, "X.gggw")
   ]),
   section("B", "floor", [
-    repeat(76.97, "0.gggw"),
-    repeat(79.39, "3.gggw"),
-    repeat(81.76, "8.gggw"),
-    repeat(84.19, "X.gggw")
+    repeat(76.93, "0.gggw"),
+    repeat(79.35, "3.gggw"),
+    repeat(81.72, "8.gggw"),
+    repeat(84.15, "X.gggw")
   ]),
   section("D", "bathroom", [
-    phrase(86.60, "0.wwww.^"),
-    phrase(88.99, "3.wwww.^"),
-    phrase(91.39, "8.wwww.^"),
-    phrase(93.80, "X.wwww.^")
+    phrase(86.56, "0.wwww.^"),
+    phrase(88.95, "3.wwww.^"),
+    phrase(91.35, "8.wwww.^"),
+    phrase(93.76, "X.wwww.^")
   ]),
   section("D", "bathroom", [
-    repeat(96.20, "0.wwww.^"),
-    repeat(98.60, "3.wwww.^"),
-    repeat(100.99, "8.wwww.^"),
-    repeat(103.39, "X.wwww.^")
+    repeat(96.16, "0.wwww.^"),
+    repeat(98.56, "3.wwww.^"),
+    repeat(100.95, "8.wwww.^"),
+    repeat(103.35, "X.wwww.^")
   ]),
   section("C", "stage", [
-    repeat(105.79, "0.gggg"),
-    repeat(108.19, "3.gggg"),
-    repeat(110.59, "8.gggg"),
-    repeat(113.00, "X.gggg")
+    repeat(105.75, "0.gggg"),
+    repeat(108.15, "3.gggg"),
+    repeat(110.55, "8.gggg"),
+    repeat(112.96, "X.gggg")
   ]),
   section("C", "stage", [
-    repeat(115.40, "0.gggg"),
-    repeat(117.79, "3.gggg"),
-    repeat(120.20, "8.gggg"),
-    repeat(122.59, "X.gggg")
+    repeat(115.36, "0.gggg"),
+    repeat(117.75, "3.gggg"),
+    repeat(120.16, "8.gggg"),
+    repeat(122.55, "X.gggg")
   ]),
   section("B", "floor", [
-    repeat(124.99, "0.gggw"),
-    repeat(127.39, "3.gggw"),
-    repeat(129.78, "8.gggw"),
-    repeat(132.20, "X.gggw")
+    repeat(124.95, "0.gggw"),
+    repeat(127.35, "3.gggw"),
+    repeat(129.74, "8.gggw"),
+    repeat(132.16, "X.gggw")
   ]),
   section("B", "floor", [
-    repeat(134.59, "0.gggw"),
-    repeat(136.99, "3.gggw"),
-    repeat(139.38, "8.gggw"),
-    repeat(141.79, "X.gggw")
+    repeat(134.55, "0.gggw"),
+    repeat(136.95, "3.gggw"),
+    repeat(139.34, "8.gggw"),
+    repeat(141.75, "X.gggw")
   ]),
   section("A", "bar", [
-    repeat(144.20, "0.wwww"),
-    repeat(146.60, "3.wwww"),
-    repeat(148.99, "8.wwww"),
-    repeat(151.39, "X.wwww")
+    repeat(144.16, "0.wwww"),
+    repeat(146.56, "3.wwww"),
+    repeat(148.95, "8.wwww"),
+    repeat(151.35, "X.wwww")
   ]),
   section("A", "bar", [
-    repeat(153.79, "0.wwww"),
-    repeat(156.20, "3.wwww"),
-    repeat(158.59, "8.wwww"),
-    repeat(160.98, "X.wwww")
+    repeat(153.75, "0.wwww"),
+    repeat(156.16, "3.wwww"),
+    repeat(158.55, "8.wwww"),
+    repeat(160.94, "X.wwww")
   ]),
   section("O", "taxi", [
-    repeat(163.39, "0.wsws"),
-    repeat(165.79, "3.wsws"),
-    repeat(168.19, "8.wsws"),
-    repeat(170.60, "X.wsws")
+    repeat(163.35, "0.wsws"),
+    repeat(165.75, "3.wsws"),
+    repeat(168.15, "8.wsws"),
+    repeat(170.56, "X.wsws")
   ]),
   section("O", "taxi", [
-    repeat(172.98, "0.wsws"),
-    repeat(175.39, "3.wsws"),
-    repeat(177.78, "8.wsws"),
-    repeat(180.20, "X.wsws", 182.99)
+    repeat(172.94, "0.wsws"),
+    repeat(175.35, "3.wsws"),
+    repeat(177.74, "8.wsws"),
+    repeat(180.16, "X.wsws", 182.95)
   ])
 ], video=False)
 
