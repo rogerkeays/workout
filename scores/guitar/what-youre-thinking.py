d5b665eb01b37307ffa4424fcd6ad5fbde292e27
Roger Keays <>
2026-08-27 15:18:14 +0800

    transcribe what you're thinking: rhythm guitar basic structure

---
+++ scores/guitar/what-youre-thinking.py
@@ -0,0 +1,94 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+guitar(2009, "what-youre-thinking", "nVl7g9b0oqk", 4, 158, "00", [ # lima-coast
+
+  # park
+  section("I", "park", [
+    phrase(0.20, "I1"),
+    phrase(3.78, "I2"),
+    phrase(6.77, "I1"),
+    phrase(9.81, "I2")
+  ]),
+
+  # cliffs
+  section("V", "clifftop", [
+    phrase(12.72, "V1"),
+    phrase(15.70, "V2"),
+    phrase(18.64, "V3"),
+    phrase(21.64, "V4")
+  ]),
+  section("V", "campfire", [
+    phrase(24.69, "V1"),
+    phrase(27.63, "V2"),
+    phrase(30.62, "V3"),
+    phrase(33.56, "V4")
+  ]),
+  section("C", "beach", [
+    phrase(36.51, "C1"),
+    phrase(39.50, "C2"),
+    phrase(42.53, "C1"),
+    phrase(45.52, "C2"),
+    phrase(48.47, "C1"),
+    phrase(51.45, "C2"),
+    phrase(54.50, "C1")
+  ]),
+
+  # path
+  section("S", "path", [
+    phrase(57.49, "C4"),
+    phrase(60.43, "C1"),
+    phrase(63.38, "C4"),
+    phrase(66.36, "C1")
+  ]),
+
+  # house
+  section("V", "dresser", [
+    phrase(69.36, "V1"),
+    phrase(72.24, "V2"),
+    phrase(75.24, "V3"),
+    phrase(78.20, "V4")
+  ]),
+  section("V", "entrance", [
+    phrase(81.18, "V1"),
+    phrase(84.15, "V2"),
+    phrase(87.09, "V3"),
+    phrase(90.08, "V4")
+  ]),
+  section("C", "veranda", [
+    phrase(93.10, "C1"),
+    phrase(96.04, "C2"),
+    phrase(99.04, "C1"),
+    phrase(102.10, "C2"),
+    phrase(105.06, "C1"),
+    phrase(108.06, "C2"),
+    phrase(111.01, "Cx"),
+    phrase(114.12, "Cx")
+  ]),
+
+  # bungalow
+  section("M", "roof", [
+    phrase(117.07, "Mx"),
+    phrase(120.02, "Mx"),
+    phrase(123.10, "Mx"),
+    phrase(126.28, "Mx")
+  ]),
+  section("M", "bedroom", [
+    phrase(129.17, "Mx"),
+    phrase(132.17, "Mx"),
+    phrase(135.18, "Mx"),
+    phrase(138.36, "Mx")
+  ]),
+
+  # stairs
+  section("C", "stairs", [
+    phrase(141.16, "C1'"),
+    phrase(144.11, "C2'"),
+    phrase(147.41, "C1'"),
+    phrase(150.64, "C2'"),
+    phrase(153.67, "C1"),
+    phrase(156.69, "Cx"),
+    phrase(159.75, "Cx"),
+    phrase(163.06, "Cx", [], 168.33)
+  ])
+], video=False)
+
