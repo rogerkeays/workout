9ed174af5f05cd510edf5c7d5fbece0ad9d36f0a
Roger Keays <>
2026-08-30 02:55:28 +0800

    transcribe minuet 2 structure and timestamps for violin

---
+++ scores/violin/minuet-2.py
@@ -0,0 +1,68 @@
+# vim: foldmethod=marker foldmarker=violin_notes(,) foldtext=getline(v\:foldstart)
+
+violin(2043, "minuet-2", "8AukGFtR5_M", 3, 120, "47", [ # show-n-tell
+  section("I", "intro", [
+    phrase(2.38, "intro", skip=True)
+  ]),
+  section("A", "front", [
+    phrase(8.41, "like"),   # /.i-like.pea-nut.but-ter/.sand.wi.^ches : 0470 2L0 00 -/// v// v-
+    repeat(11.32, "like"),
+    phrase(14.18, "lana"),  # /.la.na.makes-them/.san.dy.makes-them/ : 9990 7770 /--/ \--/
+    phrase(17.04, "i")      # /.and.i-can.make-them/.too../ : 57 5 45 2 \/\ \/\
+  ]),
+  section("a", "front", [
+    repeat(19.94, "like"),
+    repeat(22.80, "like"),
+    phrase(25.65, "sister"),  # my sister makes them my brother makes them : (those) : 9 7 5 4 27 5 4 2 0 ^\\\\ /\\\\
+    phrase(28.60, "every")    # /.e_=ve_=ry.sin^.gle/.day.. : 245 7L0 /// \//
+  ]),
+  section("A", "front", [
+    repeat(31.49, "like"),
+    repeat(34.39, "like"),
+    repeat(37.21, "lana"),
+    repeat(40.13, "i")
+  ]),
+  section("a", "front", [
+    repeat(42.92, "like"),
+    repeat(45.77, "like"),
+    repeat(48.65, "sister"),
+    repeat(51.48, "every")
+  ]),
+  section("B", "desk", [
+    phrase(54.52, "jam"),         # peanut butter and strawberry^jam, : (like) : 024 2 0 L0 99 -//\\ \/\-
+    phrase(57.38, "everything"),  # peanut butter goes with every^thing : (like) : 0L9 0L9 L44 ^\\/\ \/\- : R
+    repeat(60.23, "everything"),  # peanut butter goes with every^thing : (like) : jam.2[/9]
+    phrase(63.01, "icecream")     # [.choc_=_o=late.ice.^cream/.oh.how-i.love] : L02 489 89L /// \// \//
+  ]),
+  section("C", "class", [
+    phrase(65.93, "those"),   # /.those.pea-nut.but-ter/.sand.wich-es.that-la/ : 00L97 99754 /-\\\ /-\\\
+    phrase(68.78, "sandy"),   # /.na.and-san.dy-and/.i.can-make.too/ : 55420 L9 L7 /-\\\ \\/\
+    phrase(71.74, "you"),     # you_like [.them], i_like [.them] : (lana) : 277 477 /\- /\- 
+    phrase(74.64, "teacher")  # the teacher likes them too : 5 754 52 //\\/\ : R
+  ]),
+  section("a", "front", [
+    repeat(77.55, "like"),
+    repeat(80.38, "like"),
+    repeat(83.20, "sister"),
+    repeat(86.01, "every")
+  ]),
+  section("B", "desk", [
+    repeat(89.00, "jam"),
+    repeat(91.81, "everything"),
+    repeat(94.56, "everything"),
+    repeat(97.37, "icecream") 
+  ]),
+  section("C", "class", [
+    repeat(100.34, "those"),
+    repeat(103.10, "sandy"),
+    repeat(105.92, "you"),
+    repeat(108.86, "teacher") 
+  ]),
+  section("a", "front", [
+    repeat(111.66, "like"),
+    repeat(114.52, "like"),
+    repeat(117.43, "sister"),
+    repeat(120.28, "every", 124.73)
+  ])
+])
+
