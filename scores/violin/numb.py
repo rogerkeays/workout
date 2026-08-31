d39ebeff8d2b95283ff62311c07f0c1a17a19b43
Roger Keays <>
2026-08-31 23:56:43 +0800

    transcribe numb, kezia amelia violin version

---
+++ scores/violin/numb.py
@@ -0,0 +1,68 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+violin(3009, "numb", "G31N_11ee84", 4, 110, "49", [ # med-ball
+  section("I", "lounge", [
+    phrase(0.21, "tap", skip=True),
+    phrase(1.62, "underpants"),  # [e/las\tic /un/der\pants] : kw skxs xx : 47490L
+    phrase(5.91, "popsicles"),   # [pine/a\pple /pop\si\cles] : (underpants) : 474 0L7
+    repeat(10.35, "underpants"),
+    repeat(14.78, "popsicles")
+  ]),
+  section("V", "dinner", [
+    phrase(20.41, "being"),      # tired of been' wh-at /you \want me-e-a \to \be : xwww kwxx : 45420
+    phrase(23.96, "feeling"),    # i'm feel-ing \so \faith/less : wwk : 4202
+    phrase(26.41, "lost"),       # /un-der \the \sur/face now : k wwkx : 7 (feeling)
+    phrase(29.17, "expecting"),  # don't know what you're /ex\pecting \of \me : (tired)
+    phrase(32.70, "pressure"),   # \put /un-der \the \pre/ssure : (lost)
+    phrase(35.17, "shoes")       # /un-der \your \pre/ssured sh-oes: (lost) 4
+  ]),
+  section("B", "stairs", [
+    phrase(41.15, "step"),       # ev-ry /step that \I \take : w ww : 4974 
+    phrase(43.07, "mistake"),    # is a/no-ther \mis\take : kw kw : (step)
+    phrase(45.23, "to-you")      # /to /you : kk xxxx xxxx : 79
+  ]),
+  section("C", "stage", [
+    phrase(50.45, "numb"),       # i've be-come \so/ a-numb : sksw s : 979 
+    phrase(53.15, "feel"),       # i can't fe-el /you \there : xwws s : 9Y7
+    phrase(55.37, "tired"),      # i've be-come \so/ tired : xksw s : (numb)
+    phrase(57.49, "aware"),      # so much more \a/ware : xwss s : (numb)
+    phrase(59.68, "becoming"),   # by be-com\ing /this : (aware)
+    phrase(61.90, "to-do"),      # all i want /to \do : (aware) : (feel)
+    phrase(64.06, "like-me"),    # is be more [\li/ike] me : (aware [w]) : (numb)
+    phrase(66.22, "like-you")    # and be less \li/ike you : (like-me)
+  ]),
+  section("V", "bedroom", [
+    phrase(68.43, "smothering"), # can't you see that \you're /sm\[o-the]-ring me : (tired [w]) : 9797
+    phrase(72.26, "holding"),    # hold\ing too tight\ly : (feeling) : 974
+    phrase(74.18, "afraid"),     # a/fraid \to\ lose /co-on-trol? : (walking) : 45424
+    phrase(77.16, "everything"), # \'cause /ev-ry-thing that /you thought i-e-i \would \be : k (tired) : 9 (tired)
+    phrase(80.97, "fallen"),     # /fa-llen \a\part now : xxxk wwxx : 9420
+    phrase(82.91, "right")       # fa-llen a-part right now : (walking) : 42024
+  ]),
+  section("B", "lobby", [
+    repeat(89.15, "step"),
+    repeat(91.09, "mistake"),
+    repeat(93.23, "to-you"),
+    phrase(97.88, "second"),     # ev-ry /se-cond \i \waste : (step) : 4974
+    phrase(99.79, "take"),       # is more than /i \can /ta\ke : xxkw tkxs : 40Y0
+    phrase(102.24, "blah")       # blah..
+  ]),
+  section("C", "streets", [
+    repeat(102.81, "numb"),
+    repeat(105.50, "feel"),
+    repeat(107.72, "tired"),
+    repeat(109.90, "aware"),
+    repeat(112.05, "becoming"),
+    phrase(114.23, "to-do"),     # to -> to-do
+    repeat(116.46, "like-me"),
+    repeat(118.56, "like-you")
+  ]),
+  section("M", "lab", [
+    phrase(121.59, "know"),      # and /i \know : xxkw : 90Y
+    phrase(123.81, "failing"),   # \i /may /end\ up /fai\li\ng too : xxks sksx sxsw : 9Y0Y0Y9
+    phrase(130.34, "know"),      # but /i \know
+    phrase(132.79, "just"),      # you \were /just li-ke me, li-ke me, li-ke me : w wk : Y9Y9Y9
+    phrase(136.89, "no-good", [], 141.60) # it's no good, get-a-life
+  ])
+])
+
