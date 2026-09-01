4f0e1fd991f301b9e768c93a6b1df8ce8b0f68d4
Roger Keays <>
2026-09-01 19:23:44 +0800

    transcribe a horse with no name structure (guitar)

---
+++ scores/guitar/horse.py
@@ -0,0 +1,102 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+guitar(1026, "horse", "na47wMFfQCo", 4, 120, "44", [
+  section("I", "parade", [
+    phrase(0.45, "A"),
+    repeat(4.51, "A")
+  ]),
+  section("V", "jerusalem", [
+    repeat(8.50, "A"),
+    repeat(12.54, "A"),
+    repeat(16.51, "A"),
+    repeat(20.55, "A")
+  ]),
+  section("V", "road", [
+    repeat(24.37, "A"),
+    repeat(28.32, "A"),
+    repeat(32.23, "A"),
+    repeat(36.09, "A")
+  ]),
+  section("C", "desert", [
+    repeat(39.97, "A"),
+    repeat(43.82, "A"),
+    repeat(47.70, "A"),
+    repeat(51.54, "A")
+  ]),
+  section("D", "oasis", [
+    repeat(55.39, "A"),
+    repeat(59.22, "A"),
+    repeat(63.11, "A"),
+    repeat(66.91, "A")
+  ]),
+  section("V", "river", [
+    repeat(70.74, "A"),
+    repeat(74.55, "A"),
+    repeat(78.45, "A"),
+    repeat(82.31, "A")
+  ]),
+  section("v", "grave", [
+    repeat(86.19, "A"),
+    repeat(89.97, "A")
+  ]),
+  section("C", "desert", [
+    repeat(93.94, "A"),
+    repeat(97.74, "A"),
+    repeat(101.54, "A"),
+    repeat(105.27, "A")
+  ]),
+  section("D", "oasis", [
+    repeat(109.30, "A"),
+    repeat(113.16, "A"),
+    repeat(116.97, "A"),
+    repeat(120.81, "A")
+  ]),
+  section("S", "clouds", [
+    repeat(124.67, "A"),
+    repeat(128.61, "A"),
+    repeat(132.40, "A"),
+    repeat(136.26, "A")
+  ]),
+  section("V", "boat", [
+    repeat(140.06, "A"),
+    repeat(144.01, "A"),
+    repeat(147.82, "A"),
+    repeat(151.58, "A")
+  ]),
+  section("V", "city", [
+    repeat(155.58, "A"),
+    repeat(159.47, "A"),
+    repeat(163.32, "A"),
+    repeat(167.20, "A")
+  ]),
+  section("C", "desert", [
+    repeat(170.99, "A"),
+    repeat(174.80, "A"),
+    repeat(178.65, "A"),
+    repeat(182.46, "A")
+  ]),
+  section("D", "oasis", [
+    repeat(186.36, "A"),
+    repeat(190.17, "A"),
+    repeat(194.09, "A"),
+    repeat(197.95, "A")
+  ]),
+  section("D", "oasis", [
+    repeat(201.78, "A"),
+    repeat(205.55, "A"),
+    repeat(209.39, "A"),
+    repeat(213.24, "A")
+  ]),
+  section("D", "oasis", [
+    repeat(217.12, "A"),
+    repeat(220.94, "A"),
+    repeat(224.79, "A"),
+    repeat(228.64, "A")
+  ]),
+  section("D", "oasis", [
+    repeat(232.45, "A"),
+    repeat(236.30, "A"),
+    repeat(240.07, "A", 243.43)
+  ])
+])
+
