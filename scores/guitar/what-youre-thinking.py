8c9f824eacd1b6671cc3ae94c524d578d7410efa
Roger Keays <>
2026-09-01 18:16:58 +0800

    add repeats: what you're thinking chords

--- scores/guitar/what-youre-thinking.py
+++ scores/guitar/what-youre-thinking.py
@@ -1,6 +1,6 @@
 # vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
 
-guitar(2009, "what-youre-thinking", "nVl7g9b0oqk", 4, 158, "00", [ # lima-coast
+guitar(2009, "what-youre-thinking", "nVl7g9b0oqk", 4, 158, "45", [ # lima-coast
 
   # park
   section("I", "park", [
@@ -14,69 +14,69 @@ guitar(2009, "what-youre-thinking", "nVl7g9b0oqk", 4, 158, "00", [ # lima-coast
   section("V", "clifftop", [
     phrase(12.72, "V1"),
     phrase(15.70, "V2"),
-    phrase(18.64, "V3"),
+    phrase(18.70, "V3"),
     phrase(21.64, "V4")
   ]),
   section("V", "campfire", [
-    phrase(24.69, "V1"),
-    phrase(27.63, "V2"),
-    phrase(30.62, "V3"),
-    phrase(33.56, "V4")
+    repeat(24.69, "V1"),
+    repeat(27.63, "V2"),
+    repeat(30.62, "V3"),
+    repeat(33.56, "V4")
   ]),
   section("C", "beach", [
     phrase(36.51, "C1"),
     phrase(39.50, "C2"),
     phrase(42.53, "C1"),
     phrase(45.52, "C2"),
-    phrase(48.47, "C1"),
-    phrase(51.45, "C2"),
-    phrase(54.50, "C1")
+    phrase(48.47, "c1"),
+    phrase(51.45, "c2"),
+    phrase(54.50, "c3"),
+    phrase(57.49, "c4")
   ]),
 
   # path
   section("S", "path", [
-    phrase(57.49, "C4"),
-    phrase(60.43, "C1"),
-    phrase(63.38, "C4"),
-    phrase(66.36, "C1")
+    repeat(60.43, "I2"),
+    repeat(63.38, "I1"),
+    repeat(66.36, "I2")
   ]),
 
   # house
   section("V", "dresser", [
-    phrase(69.36, "V1"),
-    phrase(72.24, "V2"),
-    phrase(75.24, "V3"),
-    phrase(78.20, "V4")
+    repeat(69.36, "V1"),
+    repeat(72.24, "V2"),
+    repeat(75.24, "V3"),
+    repeat(78.20, "V4")
   ]),
   section("V", "entrance", [
-    phrase(81.18, "V1"),
-    phrase(84.15, "V2"),
-    phrase(87.09, "V3"),
-    phrase(90.08, "V4")
+    repeat(81.18, "V1"),
+    repeat(84.15, "V2"),
+    repeat(87.09, "V3"),
+    repeat(90.08, "V4")
   ]),
   section("C", "veranda", [
-    phrase(93.10, "C1"),
-    phrase(96.04, "C2"),
-    phrase(99.04, "C1"),
-    phrase(102.10, "C2"),
-    phrase(105.06, "C1"),
-    phrase(108.06, "C2"),
-    phrase(111.01, "Cx"),
-    phrase(114.12, "Cx")
+    repeat(93.10, "C1"),
+    repeat(96.04, "C2"),
+    repeat(99.04, "C1"),
+    repeat(102.10, "C2"),
+    repeat(105.06, "c1"),
+    repeat(108.06, "c2"),
+    repeat(111.01, "c3"),
+    repeat(114.12, "c4")
   ]),
 
   # bungalow
   section("M", "roof", [
-    phrase(117.07, "Mx"),
-    phrase(120.02, "Mx"),
-    phrase(123.10, "Mx"),
-    phrase(126.28, "Mx")
+    phrase(117.07, "M1"),
+    phrase(120.02, "M2"),
+    phrase(123.10, "M3"),
+    phrase(126.28, "M4")
   ]),
   section("M", "bedroom", [
-    phrase(129.17, "Mx"),
-    phrase(132.17, "Mx"),
-    phrase(135.18, "Mx"),
-    phrase(138.36, "Mx")
+    repeat(129.17, "M1"),
+    repeat(132.17, "M2"),
+    repeat(135.18, "M3"),
+    repeat(138.36, "M4")
   ]),
 
   # stairs
@@ -85,10 +85,10 @@ guitar(2009, "what-youre-thinking", "nVl7g9b0oqk", 4, 158, "00", [ # lima-coast
     phrase(144.11, "C2'"),
     phrase(147.41, "C1'"),
     phrase(150.64, "C2'"),
-    phrase(153.67, "C1"),
-    phrase(156.69, "Cx"),
-    phrase(159.75, "Cx"),
-    phrase(163.06, "Cx", [], 168.33)
+    repeat(153.67, "c1"),
+    repeat(156.69, "c2"),
+    repeat(159.75, "c3"),
+    phrase(163.06, "cO", [], 168.33)
   ])
 ], video=False)
 
