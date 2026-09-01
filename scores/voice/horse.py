aed30472295a95ea1f6d18a3ced9d1d7e4f5dbb0
Roger Keays <>
2026-09-01 19:26:07 +0800

    trancribe a horse with no name timestamps (voice)

---
+++ scores/voice/horse.py
@@ -0,0 +1,97 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+voice(1004, "horse", "na47wMFfQCo", 4, 120, "44", [
+  section("I", "parade", [
+    phrase(0.45, "intro", skip=True),
+  ]),
+  section("V", "jerusalem", [
+    phrase(8.01, "journey"),     # on the first part of the journey
+    phrase(12.05, "life"),       # i was looking at all the~ life
+    phrase(15.98, "plants"),     # there were plants and birds and rocks and things
+    phrase(19.84, "sand")        # there was sand and hills and rings
+  ]),
+  section("V", "road", [
+    phrase(24.27, "fly"),        # the first thing i met was a fly with a buzz
+    phrase(27.61, "sky"),        # and the sky with no clouds
+    phrase(32.04, "heat"),       # the heat was hot and the ground was dry
+    phrase(35.44, "air")         # but the air was full of sound
+  ]),
+  section("C", "desert", [
+    phrase(39.79, "horse"),      # i've been through the desert on a horse with no name
+    phrase(43.13, "rain"),       # it felt good to be out of the rain
+    phrase(47.21, "name"),       # in the desert you can remember your name
+    phrase(51.08, "pain")        # 'cause there ain't no one for to give you no pain
+  ]),
+  section("D", "oasis", [
+    phrase(55.37, "la"),         # la la, la-la-la-la
+    phrase(58.74, "lala"),       # la-la la laa la
+    repeat(63.09, "la"),
+    repeat(66.43, "lala")
+  ]),
+  section("V", "river", [
+    phrase(70.29, "two-days"),   # after two days in the desert sun
+    phrase(74.38, "skin"),       # my skin began to turn red
+    phrase(78.02, "three-days"), # after three days in the desert fun
+    phrase(81.66, "river")       # i was looking at a river bed
+  ]),
+  section("v", "grave", [
+    phrase(85.53, "story"),      # and the story it told of a river that flowed
+    phrase(89.32, "dead")        # made me sad to think it was dead
+  ]),
+  section("C", "desert", [
+    repeat(93.25, "horse"),      # [you see] ...
+    repeat(97.11, "rain"),
+    repeat(101.10, "name"),
+    repeat(104.91, "pain")
+  ]),
+  section("D", "oasis", [
+    repeat(108.91, "la"),         # [la] ...
+    repeat(112.63, "lala"),
+    repeat(116.47, "la"),
+    repeat(120.37, "lala")
+  ]),
+  section("S", "clouds", [
+    phrase(124.66, "solo", skip=True),
+  ]),
+  section("V", "boat", [
+    phrase(139.63, "nine-days"),  # after nine days i let the horse run free
+    phrase(143.48, "sea"),        # 'cause the desert had turned to sea
+    repeat(147.36, "plants"),
+    repeat(151.07, "sand")
+  ]),
+  section("V", "city", [
+    phrase(155.42, "ocean"),      # the ocean is a desert with it's life underground
+    phrase(158.93, "disguise"),   # and a perfect disguise above
+    phrase(162.68, "cities"),     # under the cities lies a heart made of ground
+    phrase(166.49, "humans")      # but the humans will give no love
+  ]),
+  section("C", "desert", [
+    repeat(170.32, "horse"),
+    repeat(174.16, "rain"),
+    repeat(178.04, "name"),
+    repeat(182.00, "pain")
+  ]),
+  section("D", "oasis", [
+    repeat(186.00, "la"),
+    repeat(189.74, "lala"),
+    repeat(193.59, "la"),
+    repeat(197.40, "lala")
+  ]),
+  section("D", "oasis", [
+    repeat(201.31, "la"),
+    repeat(205.09, "lala"),
+    repeat(208.89, "la"),
+    repeat(212.77, "lala")
+  ]),
+  section("D", "oasis", [
+    repeat(217.09, "la"),  # [-la]
+    repeat(220.42, "lala"),
+    repeat(224.30, "la"),
+    repeat(228.07, "lala")
+  ]),
+  section("D", "oasis", [
+    repeat(232.01, "la"),
+    repeat(235.78, "lala", 243.04)
+  ])
+])
+
