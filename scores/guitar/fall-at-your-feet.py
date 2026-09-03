e9835761be6c87dd6c32e4174c7a5f6b92a9dc4b
Roger Keays <>
2026-09-03 20:24:18 +0800

    transcribe fall at your feet rhythm guitar structure

---
+++ scores/guitar/fall-at-your-feet.py
@@ -0,0 +1,44 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+guitar(3042, "fall-at-your-feet", "2ROiU9kOHeE", 4, 98, 43, [ # canberra
+  section("I", "her-office", [
+    phrase(0.85, "I1"),
+    repeat(5.57, "I1"),
+    repeat(10.29, "I1")
+  ]),
+  section("V", "his-room", [
+    phrase(15.06, "V1"),
+    phrase(24.46, "V2"),
+    phrase(32.70, "V3")
+  ]),
+  section("C", "temple", [
+    phrase(42.10, "C1"),
+    repeat(51.50, "C1"),
+    repeat(60.82, "C1")
+  ]),
+  section("V", "her-doorstop", [
+    repeat(70.09, "V1"),
+    phrase(79.45, "V2-"),
+    repeat(86.43, "V3")
+  ]),
+  section("C", "temple", [
+    repeat(95.80, "C1"),
+    repeat(105.18, "C1"),
+    repeat(114.58, "C1")
+  ]),
+  section("M", "pool", [
+    repeat(123.85, "C1"),
+    repeat(133.17, "C1")
+  ]),
+  section("S", "diving", [
+    repeat(142.78, "C1"),
+    repeat(151.73, "C1")
+  ]),
+  section("C", "temple", [
+    repeat(160.79, "C1"),
+    repeat(170.31, "C1"),
+    repeat(179.70, "C1"),
+    phrase(188.86, "O", stop=195.49)
+  ])
+])
+
