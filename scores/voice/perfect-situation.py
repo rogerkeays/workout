c55e6c95f9b735734bf34289cbf09e140e8aac80
Roger Keays <>
2026-09-10 00:26:21 +0800

    transcribe perfect situation structure and voice timestamps

---
+++ scores/voice/perfect-situation.py
@@ -0,0 +1,79 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+voice(2011, "perfect-situation", "DW-EWpgFTrI", 4, 94, "00", [ # hospital
+  section("I", "change-rooms", [
+    phrase(0.14, "intro", skip=True),
+  ]),
+  section("V", "baseball-pitch", [
+    phrase(40.53, "deal"),     # what's the deal with my brain?
+    phrase(43.09, "insane"),   # why am i so obviously insane?
+    phrase(45.83, "perfect"),  # in a perfect situation
+    phrase(48.71, "drain"),    # i let love down the drain
+    phrase(50.75, "pitch"),    # there's the pitch, slow and straight
+    phrase(53.32, "swing"),    # all i have to do is swing and i'm a hero
+    phrase(58.28, "zero")      # but i'm a zero
+  ]),
+  section("V", "club", [
+    phrase(60.93, "hungry"),        # hungry nights, once again
+    phrase(63.50, "unbelievable"),  # now it's getting unbelievable
+    phrase(66.23, "cause"),         # cause i could not have it better
+    phrase(68.80, "play"),          # but i just can't get no play
+    phrase(71.14, "girls"),         # from the girls all around
+    phrase(73.71, "search"),        # as they search the night for someone to hold onto
+    phrase(78.98, "pass")           # i just pass through
+  ]),
+  section("C", "ambulance", [
+    phrase(81.27, "singing"),  # singing, ah-ho
+    phrase(84.58, "ah-ho"),    # ah, ho
+    phrase(87.18, "ha-o-o"),   # oh, ha-o-o-oh
+    repeat(91.55, "singing"),
+    repeat(94.80, "ah-ho"),
+    repeat(97.36, "ha-o-o")
+  ]),
+  section("V", "ward", [
+    phrase(101.79, "hands"),       # get your hands off the girl
+    phrase(104.39, "belongs"),     # can't you see that she belongs to me?
+    phrase(107.17, "appreciate"),  # and i don't appreciate this
+    phrase(110.15, "excess"),      # excess company
+    phrase(112.05, "satisfy"),     # though i can't satisfy
+    phrase(114.62, "needs"),       # all the needs she has and so she starts to wander
+    phrase(119.87, "blame")        # can you blame her?
+  ]),
+  section("C", "ambulance", [
+    repeat(122.07, "singing"),
+    repeat(125.47, "ah-ho"),
+    repeat(128.08, "ha-o-o"),
+    repeat(132.37, "singing"),
+    repeat(135.72, "ah-ho"),
+    repeat(138.26, "ha-o-o")
+  ]),
+  section("B", "shrinks", [
+    phrase(143.92, "logic"),    # tell me there's a logic out there
+    phrase(146.50, "prepare"),  # leading me to better prepare
+    phrase(149.13, "special"),  # for the day that something really special might come
+    phrase(154.24, "hope"),     # tell me there's some hope for me
+    phrase(156.92, "lonely"),   # i don't wanna be lonely
+    phrase(159.36, "earth")     # for the rest of my days on the earth, oh
+  ]),
+  section("S", "yard", [
+    phrase(163.93, "solo", skip=True),
+  ]),
+  section("C", "ambulance", [
+    phrase(184.56, "singing-"), # (drop "singing")
+    repeat(187.10, "ah-ho"),
+    repeat(189.71, "ha-o-o"),
+    repeat(194.01, "singing"),
+    repeat(197.24, "ah-ho"),
+    repeat(199.76, "ha-o-o")
+  ]),
+  section("O", "shock-shop", [
+    phrase(204.07, "oooh"),     # singing oh oooh, wo-o-ah, ah-ah
+    phrase(213.50, "woooh")     # wo-o-oh a-woooh, wo-o-ah, ah-ah
+  ]),
+  section("O", "trundle-bed", [
+    phrase(223.75, "wo-o-ah"),  # wo-o-ah, wo-o-ah, wo-o-ah, wo-o-ah
+    repeat(233.87, "wo-o-ah"),
+    phrase(244.02, "wo-o-oh", [], 253.88)   # wo-o-oh : wo-o-oh
+  ])
+])
+
