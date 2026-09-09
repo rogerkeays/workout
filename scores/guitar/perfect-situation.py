3b6b81636a6046a840ee9bf8308bcb9531bdfe04
Roger Keays <>
2026-09-10 01:06:22 +0800

    transcribe perfect situation guitar timestamps

---
+++ scores/guitar/perfect-situation.py
@@ -0,0 +1,51 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+guitar(2043, "perfect-situation", "_Tx0dKij8Zs", 4, 94, "00", [ # boston
+  section("I", "change-rooms", [
+    phrase(6.54, "I1"),
+    phrase(16.87, "i1"),
+    phrase(27.03, "I2"),
+    phrase(37.37, "i2")
+  ]),
+  section("V", "baseball-pitch", [
+    phrase(47.66, "V1"),
+    phrase(57.93, "v1")
+  ]),
+  section("V", "club", [
+    phrase(68.13, "V2"),
+    phrase(78.35, "v2")
+  ]),
+  section("C", "ambulance", [
+    phrase(88.73, "C1"),
+    phrase(98.83, "c1")
+  ]),
+  section("V", "ward", [
+    phrase(109.11, "V3"),
+    phrase(119.32, "v3")
+  ]),
+  section("C", "ambulance", [
+    phrase(129.54, "C2"),
+    phrase(139.82, "c2")
+  ]),
+  section("B", "shrinks", [
+    phrase(150.02, "B1"),
+    phrase(160.30, "b1")
+  ]),
+  section("S", "yard", [
+    phrase(170.64, "S1"),
+    phrase(181.04, "s1")
+  ]),
+  section("C", "ambulance", [
+    phrase(191.37, "C3"),
+    phrase(201.54, "c3")
+  ]),
+  section("O", "shock-shop", [
+    phrase(211.59, "O1"),
+    phrase(221.84, "o1")
+  ]),
+  section("O", "trundle-bed", [
+    phrase(232.22, "O2"),
+    phrase(242.30, "o2", [], 259.27)
+  ])
+])
+
