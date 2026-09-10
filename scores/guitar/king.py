5497d967f4df787de34b9f39ea4b596c134b31db
Roger Keays <>
2026-09-11 00:27:40 +0800

    transcribe king structure for guitar

---
+++ scores/guitar/king.py
@@ -0,0 +1,42 @@
+# vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
+
+guitar(2035, "king", "KwXNHdfjh30", 4, 62.5, "49", [ # fridays
+  section("V", "club-bar", [
+    phrase(1.72, "V1"),
+    phrase(16.96, "v1")
+  ]),
+  section("V", "alcove", [
+    phrase(32.10, "V2"),
+    phrase(47.26, "v2")
+  ]),
+  section("C", "behind-bar", [
+    phrase(62.58, "C1"),
+    phrase(77.92, "c1")
+  ]),
+  section("V", "beergarden", [
+    phrase(92.93, "V3"),
+    phrase(107.99, "v3")
+  ]),
+  section("V", "vip", [
+    phrase(123.09, "V4"),
+    phrase(138.18, "v4")
+  ]),
+  section("C", "behind-bar", [
+    phrase(153.39, "C2"),
+    phrase(166.60, "c2")
+  ]),
+  section("M", "stairway", [
+    phrase(183.70, "M1"),
+    phrase(199.01, "m2")
+  ]),
+  section("C", "behind-bar", [
+    phrase(214.08, "C3"),
+    phrase(229.35, "c3")
+  ]),
+  section("O", "exit", [
+    phrase(244.41, "o1"),
+    phrase(259.60, "s1"),
+    phrase(274.62, "o2", [], 303.55)
+  ])
+], video=False)
+
