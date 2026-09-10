28c1ebec940401fff11c93cea8f7069a65359fdb
Roger Keays <>
2026-09-11 00:28:13 +0800

    perfect situation guitar: add repeats

--- scores/guitar/perfect-situation.py
+++ scores/guitar/perfect-situation.py
@@ -1,51 +1,51 @@
 # vim: foldmethod=marker foldmarker=notes(,) foldtext=getline(v\:foldstart)
 
-guitar(1043, "perfect-situation", "_Tx0dKij8Zs", 4, 94, "00", [ # boston
+guitar(1043, "perfect-situation", "_Tx0dKij8Zs", 4, 94, "36", [ # boston
   section("I", "change-rooms", [
     phrase(6.54, "I1"),
-    phrase(16.87, "i1"),
-    phrase(27.03, "I2"),
-    phrase(37.37, "i2")
+    repeat(16.87, "I1"),
+    repeat(27.03, "I1"),
+    repeat(37.37, "I1")
   ]),
   section("V", "baseball-pitch", [
-    phrase(47.66, "V1"),
-    phrase(57.93, "v1")
+    repeat(47.66, "I1"),
+    repeat(57.93, "I1")
   ]),
   section("V", "club", [
-    phrase(68.13, "V2"),
-    phrase(78.35, "v2")
+    repeat(68.13, "I1"),
+    repeat(78.35, "I1")
   ]),
   section("C", "ambulance", [
-    phrase(88.73, "C1"),
-    phrase(98.83, "c1")
+    repeat(88.73, "I1"),
+    repeat(98.83, "I1")
   ]),
   section("V", "ward", [
-    phrase(109.11, "V3"),
-    phrase(119.32, "v3")
+    repeat(109.11, "I1"),
+    repeat(119.32, "I1")
   ]),
   section("C", "ambulance", [
-    phrase(129.54, "C2"),
-    phrase(139.82, "c2")
+    repeat(129.54, "I1"),
+    repeat(139.82, "I1")
   ]),
   section("B", "shrinks", [
     phrase(150.02, "B1"),
-    phrase(160.30, "b1")
+    repeat(160.30, "B1")
   ]),
   section("S", "yard", [
-    phrase(170.64, "S1"),
-    phrase(181.04, "s1")
+    repeat(170.64, "I1"),
+    repeat(181.04, "I1")
   ]),
   section("C", "ambulance", [
-    phrase(191.37, "C3"),
-    phrase(201.54, "c3")
+    repeat(191.37, "I1"),
+    repeat(201.54, "I1")
   ]),
   section("O", "shock-shop", [
-    phrase(211.59, "O1"),
-    phrase(221.84, "o1")
+    repeat(211.59, "I1"),
+    repeat(221.84, "I1")
   ]),
   section("O", "trundle-bed", [
-    phrase(232.22, "O2"),
-    phrase(242.30, "o2", [], 259.27)
+    repeat(232.22, "I1"),
+    repeat(242.30, "I1", [], 259.27)
   ])
 ])
 
