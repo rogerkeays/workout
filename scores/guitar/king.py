bd1a94967f5bbf7848f2b423f078f69615f2bef3
Roger Keays <>
2026-09-11 17:57:25 +0800

    king: add repeats

--- scores/guitar/king.py
+++ scores/guitar/king.py
@@ -3,40 +3,40 @@
 guitar(2035, "king", "KwXNHdfjh30", 4, 62.5, "49", [ # fridays
   section("V", "club-bar", [
     phrase(1.72, "V1"),
-    phrase(16.96, "v1")
+    repeat(16.96, "V1")
   ]),
   section("V", "alcove", [
-    phrase(32.10, "V2"),
-    phrase(47.26, "v2")
+    repeat(32.10, "V1"),
+    repeat(47.26, "V1")
   ]),
   section("C", "behind-bar", [
-    phrase(62.58, "C1"),
-    phrase(77.92, "c1")
+    repeat(62.58, "V1"),
+    repeat(77.92, "V1")
   ]),
   section("V", "beergarden", [
-    phrase(92.93, "V3"),
-    phrase(107.99, "v3")
+    repeat(92.93, "V1"),
+    repeat(107.99, "V1")
   ]),
   section("V", "vip", [
-    phrase(123.09, "V4"),
-    phrase(138.18, "v4")
+    repeat(123.09, "V1"),
+    repeat(138.18, "V1")
   ]),
   section("C", "behind-bar", [
-    phrase(153.39, "C2"),
-    phrase(166.60, "c2")
+    repeat(153.39, "V1"),
+    repeat(166.60, "V1")
   ]),
   section("M", "stairway", [
-    phrase(183.70, "M1"),
-    phrase(199.01, "m2")
+    repeat(183.70, "V1"),
+    repeat(199.01, "V1")
   ]),
   section("C", "behind-bar", [
-    phrase(214.08, "C3"),
-    phrase(229.35, "c3")
+    repeat(214.08, "V1"),
+    repeat(229.35, "V1")
   ]),
   section("O", "exit", [
-    phrase(244.41, "o1"),
-    phrase(259.60, "s1"),
-    phrase(274.62, "o2", [], 303.55)
+    repeat(244.41, "V1"),
+    repeat(259.60, "V1"),
+    repeat(274.62, "V1", [], 303.55)
   ])
 ], video=False)
 
